21b8d81c5b127e90439e82d7afd88c461f9e78ae
Hubol <hubol.gordon@gmail.com>
2023-10-17 18:34:38 -0500

    ICanvas -> HTMLCanvasElement

--- src/lib/game-engine/game-engine.ts
+++ src/lib/game-engine/game-engine.ts
@@ -1,16 +1,21 @@
-import {autoDetectRenderer, Container, ICanvas, IRenderer} from "pixi.js";
+import {autoDetectRenderer, Container, IRenderer} from "pixi.js";
 import {Animator} from "./animator";
+
+type HtmlCanvasRenderer = IRenderer<HTMLCanvasElement>; 
 
 export class GameEngine {
     readonly stage = new Container();
-    readonly render: IRenderer<ICanvas>['render'];
-    readonly canvasElement: IRenderer<ICanvas>['view'];
+    readonly render: HtmlCanvasRenderer['render'];
+    readonly canvasElement: HTMLCanvasElement;
+
+    private readonly _renderer: HtmlCanvasRenderer;
 
     constructor(
         readonly animator: Animator,
-        rendererOptions: Parameters<typeof autoDetectRenderer>[0]) {
-            const renderer = autoDetectRenderer(rendererOptions);
-            this.render = renderer.render.bind(renderer);
-            this.canvasElement = renderer.view;
+        rendererOptions: Parameters<typeof autoDetectRenderer<HTMLCanvasElement>>[0]) {
+            this._renderer = autoDetectRenderer(rendererOptions);
+            this.render = this._renderer.render.bind(this._renderer);
+            this.canvasElement = this._renderer.view;
+            console.log(`GameEngine`, this);
     }
 }
