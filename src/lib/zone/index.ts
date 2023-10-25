36dfc6916e9269907f729527b94527924e54e86f
Hubol <hubol.gordon@gmail.com>
2023-10-25 15:09:58 -0500

    ts-node(?) does not handle static properties well

--- src/lib/zone/index.ts
+++ src/lib/zone/index.ts
@@ -1,20 +1,21 @@
 // Adapted from
 // https://github.com/vlcn-io/model/blob/7d3850fc5d1f0b2ed52d721edb898fccc21cb90f/ts/packages/zone/src/index.ts
 
-import { newScope, PSD } from "./dexie/helpers/promise.js";
+import { newScope, PSD } from "./dexie/helpers/promise";
 
 type InternalContext = { key: string };
 
+const _contexts: Record<string, object> = {};
+
 export class Zone<TContext extends object> {
   private _contextsCreatedCount = 0;
-  private static readonly _contexts: Record<string, object> = {};
 
   constructor(readonly name: string) {
 
   }
 
   get context() {
-    return Zone._contexts[(PSD as InternalContext).key] as TContext;
+    return _contexts[(PSD as any as InternalContext).key] as TContext;
   }
 
   async run(fn: () => unknown, context: TContext) {
@@ -23,11 +24,11 @@ export class Zone<TContext extends object> {
 
     const internalContext: InternalContext = { key };
 
-    Zone._contexts[key] = context;
+    _contexts[key] = context;
     const scope: Promise<void> = newScope(fn, internalContext);
     await scope;
     // TODO try/catch ?
-    delete Zone._contexts[key];
+    delete _contexts[key];
   }
 
 }
