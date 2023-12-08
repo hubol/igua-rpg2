3f38bf1206bb7bbb40bdf7c9870a965de7650133
Hubol <hubol.gordon@gmail.com>
2023-12-08 17:45:57 -0600

    Extract Pojo from merge

--- src/lib/object/merge.ts
+++ src/lib/object/merge.ts
@@ -1,10 +1,4 @@
-// Crude attempt to allow only objects that are not class instances
-// I have added some keys from common class instances to try to enforce this at compile time
-type Pojo = Record<string, any> & {
-    _x?: undefined; // PixiJS ObservablePoint
-    _bounds?: undefined; // PixiJS DisplayObject
-    _parentID?: undefined; // PixiJS Transform
-}
+import { Pojo } from "../types/pojo";
 
 export function merge<T, U extends Pojo>(target: T, source: U): T & U {
     return Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) as T & U;
---
+++ src/lib/types/pojo.ts
@@ -0,0 +1,7 @@
+// Crude attempt to allow only objects that are not class instances
+// I have added some keys from common class instances to try to enforce this at compile time
+export type Pojo = Record<string, any> & {
+    _x?: undefined; // PixiJS ObservablePoint
+    _bounds?: undefined; // PixiJS DisplayObject
+    _parentID?: undefined; // PixiJS Transform
+}
