2e54f3f2848575ff2512a35664d1e5c3c1860f8c
Hubol <hubol.gordon@gmail.com>
2023-12-10 16:39:59 -0600

    Extend pixi.js Point as a Vector

--- src/lib/extensions/pixi-vector.ts
+++ src/lib/extensions/pixi-vector.ts
@@ -1,10 +1,12 @@
-import { DisplayObject, ObservablePoint } from "pixi.js";
+import { DisplayObject, ObservablePoint, Point } from "pixi.js";
 import { Vector, defineVectorProperties } from "../math/vector-type";
 
 declare module "pixi.js" {
     interface DisplayObject extends Vector { }
     interface ObservablePoint extends Vector { }
+    interface Point extends Vector { }
 }
 
 defineVectorProperties(DisplayObject.prototype, { omit: [ 'scale' ] });
 defineVectorProperties(ObservablePoint.prototype);
+defineVectorProperties(Point.prototype);
