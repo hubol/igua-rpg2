346b24512284e1da346e25ff1b0226c5642130ae
Hubol <hubol.gordon@gmail.com>
2023-12-15 23:41:37 -0600

    Create SceneLibrary

--- src/igua/find-scene-by-name.ts
+++ src/igua/find-scene-by-name.ts
@@ -1,13 +1,30 @@
 const { default: sceneExports } = require(`./scenes/**/*.ts`);
 
+type SceneLibrary = Record<string, () => unknown>;
+let sceneLibrary: SceneLibrary;
+
 export function findSceneByName(name: string): () => unknown {
+    if (!sceneLibrary) {
+        sceneLibrary = createSceneLibrary();
+        console.log('SceneLibrary', sceneLibrary);
+    }
+
+    const scene = sceneLibrary[name];
+    if (scene)
+        return scene;
+    
+    throw new Error(`Could not find Scene with name ${name}`);
+}
+
+function createSceneLibrary(): SceneLibrary {
+    const sceneLibrary = {};
     for (const exports of sceneExports) {
         for (const key in exports) {
             const fn = exports[key];
-            if (typeof fn === 'function' && fn.name === name)
-                return fn;
+            if (typeof fn === 'function')
+                sceneLibrary[key] = fn;
         }
     }
 
-    throw new Error(`Could not find Scene with name ${name}`);
+    return sceneLibrary;
 }
