567072bac102a899334ee4267381745faf16bea3
Hubol <hubol.gordon@gmail.com>
2024-01-03 12:27:45 -0600

    Use @parcel/watcher for faster change detection

--- esbuild.mjs
+++ esbuild.mjs
@@ -30,6 +30,14 @@ function options(overrides) {
     return options;
 }
 
+const trimToRelativePath = (() => {
+    const dirLength = process.cwd().length + 1;
+    /**
+     * @param { string } path
+     */
+    return (path) => path.substring(dirLength);
+})();
+
 if (!serve) {
     // Naive cache busting
     const signature = Date.now();
@@ -54,6 +62,130 @@ else {
         define: { IS_PRODUCTION: 'false' }
     }));
 
-    await ctx.watch();
+    await watch(ctx);
     await ctx.serve({ servedir: 'public' });
+    while (true) {
+        await new Promise(r => setTimeout(r, 5_000));
+        await ctx.rebuild();
+    }
+}
+
+/**
+ * 
+ * @param {import('esbuild').BuildContext<import('esbuild').BuildOptions>} ctx 
+ */
+async function watch(ctx) {
+    const rebuildFn = makeEnqueuedPromiseFn("Rebuild", () => ctx.rebuild());
+
+    if (await startParcelWatcher(rebuildFn)) {
+        console.log("Started @parcel/watcher");
+        await rebuildFn();
+    }
+    else {
+        console.log("Starting esbuild watch");
+        await ctx.watch();
+    }
+}
+
+/**
+ * @param {string} name 
+ * @param {() => Promise<unknown>} promiseFn
+ */
+function makeEnqueuedPromiseFn(name, promiseFn) {
+    let isEnqueued = false;
+    let isRunning = false;
+
+    return async () => {
+        if (isEnqueued) {
+            console.log(`${name} already enqueued, skipping...`);
+            return;
+        }
+        else if (isRunning) {
+            console.log(`Enqueueing ${name}...`);
+            isEnqueued = true;
+        }
+        
+        await wait(() => !isRunning);
+
+        console.log(`Running ${name}...`);
+        isRunning = true;
+        isEnqueued = false;
+
+        try {
+            await promiseFn();
+        }
+        catch (e) {
+            console.error(`Error during ${name}\n`, e);
+        }
+
+        isRunning = false;
+    };
+}
+
+/**
+ * 
+ * @param {() => boolean} predicate 
+ */
+function wait(predicate) {
+    if (predicate())
+        return;
+
+    let interval;
+
+    return new Promise(r => {
+        interval = setInterval(() => {
+            if (predicate())
+                r();
+        });
+    })
+    .finally(() => clearInterval(interval));
+}
+
+/**
+ * 
+ * @param {{ path: string, type: string }[]} events 
+ */
+function printEvents(events) {
+    const pathsByType = {};
+    for (const { type, path } of events) {
+        if (!pathsByType[type])
+            pathsByType[type] = [];
+
+        pathsByType[type].push(trimToRelativePath(path));
+    }
+
+    const created = pathsByType["create"] ? "create: " + pathsByType["create"].join(", ") + "\n" : "";
+    const updated = pathsByType["update"] ? "update: " + pathsByType["update"].join(", ") + "\n" : "";
+    const deleted = pathsByType["delete"] ? "delete: " + pathsByType["delete"].join(", ") + "\n" : "";
+
+    let body = created + updated + deleted;
+    if (!body)
+        body = "<No events>";
+
+    console.log("Change detected:\n" + body.trim() + "\n");
+}
+
+/**
+ * @param {Function} rebuildFn 
+ */
+async function startParcelWatcher(rebuildFn) {
+    try {
+        const { subscribe } = await import("./.smooch/native-deps/node_modules/@parcel/watcher/index.js");
+        await subscribe(
+            "./src",
+            (err, events) => {
+                if (err) {
+                    console.error("Got error from @parcel/watcher\n", err);
+                    return;
+                }
+
+                printEvents(events);
+                setImmediate(rebuildFn);
+            });
+        return true;
+    }
+    catch (e) {
+        console.warn("Could not start @parcel/watcher. It should have been installed by smooch.\n", e);
+        return false;
+    }
 }
