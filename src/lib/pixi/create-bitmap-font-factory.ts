e88258700e9a3fb4288a54e15d8fd1b75f5aae48
Hubol <hubol.gordon@gmail.com>
2024-01-14 21:00:19 -0600

    Untested fnt format preprocessor

--- src/lib/pixi/create-bitmap-font-factory.ts
+++ src/lib/pixi/create-bitmap-font-factory.ts
@@ -1,7 +1,17 @@
 import { BitmapFont, Texture } from "pixi.js";
 
 export async function createBitmapFontFactory(fntUrl: string) {
-    const fntData = await fetch(fntUrl).then(x => x.text());
+    const fntData = await fetch(fntUrl).then(x => x.text()).then(preprocessFntText);
 
     return (texture: Texture) => BitmapFont.install(fntData, texture);
+}
+
+const atSignRegex = /(?:@(.))/gm;
+const atSignSubstitution = (_: string, y: string) => String(y.charCodeAt(0));
+
+// Allows writing e.g. @a and replacing with 97
+function preprocessFntText(fntText: string) {
+    if (!fntText.includes("info NeedsPreprocessing"))
+        return fntText;
+    return fntText.replace(atSignRegex, atSignSubstitution)
 }
