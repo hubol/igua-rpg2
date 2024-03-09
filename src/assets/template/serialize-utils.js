d4f92378e67ae7362c795b9f34c46074703539c2
Hubol <hubol.gordon@gmail.com>
2024-03-09 14:33:24 -0600

    Extract utilities from template/textures.js

---
+++ src/assets/template/serialize-utils.js
@@ -0,0 +1,77 @@
+const LiteralSymbol = Symbol('Literal');
+
+function literal(text) {
+    return {
+        text,
+        __isLiteral: LiteralSymbol,
+    }
+}
+
+const LiteralTokenStart = '@_LITERAL_START_@';
+const LiteralTokenDoubleQuote = '@_LITERAL_DQUOTE_@';
+const LiteralTokenEnd = '@_LITERAL_END_@';
+
+const DoubleQuoteRegExp = /"/gm;
+const DoubleQuoteSubstitutionRegExp = /@_LITERAL_DQUOTE_@/gm;
+
+function replacer(_, value) {
+    if (value.__isLiteral === LiteralSymbol) {
+        return LiteralTokenStart + value.text.replace(DoubleQuoteRegExp, LiteralTokenDoubleQuote) + LiteralTokenEnd;
+    }
+
+    return value;
+}
+
+const LiteralRegExp = /"@_LITERAL_START_@(.*?)@_LITERAL_END_@"/gm;
+const substituteLiteral = (_, group) => group;
+
+function serialize(value) {
+    const stringified = JSON.stringify(value, replacer, 1);
+    return stringified
+        .replace(DoubleQuoteSubstitutionRegExp, '"')
+        .replace(LiteralRegExp, substituteLiteral);
+}
+
+function createTree() {
+    const tree = { };
+
+    /**
+     * 
+     * @param {string[]} path 
+     * @param {Record<string, any>} object 
+     */
+    function node(path, object) {
+        let at = tree;
+
+        for (let i = 0; i < path.length; i += 1) {
+            const component = path[i];
+
+            if (!at[component])
+                at[component] = i === path.length - 1 ? object : { };
+            at = at[component];
+        }
+    }
+
+    return {
+        tree,
+        node,
+    }
+}
+
+/**
+ * 
+ * @param {string} name
+ * @param {any[]} args
+ */
+function functionCall(name, ...args) {
+    return literal(`${name}(${args.map(JSON.stringify).join(",")})`);
+}
+
+const Literals = {
+    functionCall,
+}
+
+exports.literal = literal;
+exports.serialize = serialize;
+exports.createTree = createTree;
+exports.Literals = Literals;
--- src/assets/template/textures.js
+++ src/assets/template/textures.js
@@ -1,3 +1,5 @@
+const { serialize, createTree, Literals } = require("./serialize-utils");
+
 /**
 @param {import("@hubol/smooch/template-api").TemplateContext.TexturePack} context;
 @param {import("@hubol/smooch/template-api").Utils} utils;
@@ -14,21 +16,18 @@ module.exports = function ({ atlases, textures }, { pascal, noext, format }) {
     for (const tx of textures) {
         const path = tx.fileName.split('/').filter(x => !!x);
         const fileName = noext(path[path.length - 1]);
-        const metadata = getMetadata(fileName); // TODO remove probably
 
-        const leafName = metadata ? metadata.name : fileName;
-
-        path[path.length - 1] = leafName;
+        path[path.length - 1] = fileName;
 
         const id = path.map(pascal).join(".");
         const object = { id, atlas: atlasIndices[tx.atlasFileName], x: tx.x, y: tx.y, width: tx.width, height: tx.height };
 
-        const call = serializeCall('tx', object);
+        const call = Literals.functionCall('tx', object);
 
         node(path.map(pascal), call);
     }
 
-    const stringifiedTree = stringifyTree(tree, 2);
+    const stringifiedTree = serialize(tree);
 
     const source = `
 // This file is generated
@@ -55,66 +54,4 @@ export const GeneratedTextureData = {
 `;
 
     return format(source, { parser: 'typescript', printWidth: 2000 });
-}
-
-function serializeCall(fn, args) {
-    return {
-        fn,
-        args,
-    }
-}
-
-function stringifyTree(tree, indent = 0, root = true) {
-    if (tree._branch || root) {
-        return `{
-${Object.entries(tree).filter(([ key ]) => key !== '_branch').map(([ key, value ]) => `"${key}": ${stringifyTree(value, indent + 1, false)},`).join('\n')}
-}`
-    }
-
-    return stringifyLeaf(tree);
-}
-
-function stringifyLeaf({ fn, args }) {
-    return fn + '(' + JSON.stringify(args) + ')';    
-}
-
-function createTree() {
-    const tree = { };
-
-    /**
-     * 
-     * @param {string[]} path 
-     * @param {Record<string, any>} object 
-     */
-    function node(path, object) {
-        let at = tree;
-
-        for (let i = 0; i < path.length; i += 1) {
-            const component = path[i];
-
-            if (!at[component])
-                at[component] = i === path.length - 1 ? object : { _branch: true };
-            at = at[component];
-        }
-    }
-
-    return {
-        tree,
-        node,
-    }
-}
-
-/**
- * 
- * @param {string} fileName 
- */
-function getMetadata(fileName) {
-    if (fileName.indexOf("_") === -1)
-        return undefined;
-
-    const components = fileName.split("_");
-    return {
-        name: components[0],
-        subimages: Number(components[1]),
-    }
 }
