f2a02bf4abbb4eae5080ab745d6c1756c162a982
Hubol <hubol.gordon@gmail.com>
2024-03-17 17:37:48 -0500

    Use object literals in default looks

--- src/igua/iguana/get-default-looks.ts
+++ src/igua/iguana/get-default-looks.ts
@@ -4,21 +4,21 @@ export function getDefaultLooks(): IguanaLooks.Serializable {
     return {
         head: {
             color: 0xCCAE0A,
-            placement: [0, 0],
+            placement: { x: 0, y: 0 },
             crest: {
                 shape: 0,
                 color: 0xCC2C42,
-                placement: [0, 0],
+                placement: { x: 0, y: 0 },
                 flipV: false,
                 flipH: false,
             },
             eyes: {
-                placement: [0, 0],
+                placement: { x: 0, y: 0 },
                 gap: 1,
                 tilt: 0,
                 pupils: {
                     mirrored: true,
-                    placement: [0, 0],
+                    placement: { x: 0, y: 0 },
                 },
                 left: {
                     sclera: {
@@ -31,7 +31,7 @@ export function getDefaultLooks(): IguanaLooks.Serializable {
                     pupil: {
                         shape: 0,
                         color: 0x9957AF,
-                        placement: [0, 0],
+                        placement: { x: 0, y: 0 },
                         flipH: false,
                     }
                 },
@@ -46,7 +46,7 @@ export function getDefaultLooks(): IguanaLooks.Serializable {
                     pupil: {
                         shape: 0,
                         color: 0x9957AF,
-                        placement: [0, 0],
+                        placement: { x: 0, y: 0 },
                         flipH: false,
                     }
                 },
@@ -54,26 +54,26 @@ export function getDefaultLooks(): IguanaLooks.Serializable {
             horn: {
                 shape: -1,
                 color: 0xCC2C42,
-                placement: [0, 0]
+                placement: { x: 0, y: 0 }
             },
             mouth: {
                 shape: 0,
                 color: 0x9957AF,
-                placement: [0, 0],
+                placement: { x: 0, y: 0 },
                 flipV: false,
             },
         },
         body: {
             color: 0xCC70BB,
-            placement: [0, 0],
+            placement: { x: 0, y: 0 },
             tail: {
                 shape: 0,
                 color: 0xCC70BB,
-                placement: [0, 0],
+                placement: { x: 0, y: 0 },
                 club: {
                     shape: -1,
                     color: 0x0C4CCC,
-                    placement: [0, 0],
+                    placement: { x: 0, y: 0 },
                 }
             }
         },
