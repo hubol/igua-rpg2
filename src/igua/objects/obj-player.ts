9c57bb52bf80084d39ea08dabccae047360b504a
Hubol <hubol.gordon@gmail.com>
2024-03-17 20:52:06 -0500

    Player test can duck

--- src/igua/objects/obj-player.ts
+++ src/igua/objects/obj-player.ts
@@ -17,15 +17,20 @@ export function objPlayer(looks: IguanaLooks.Serializable) {
     const puppet = objIguanaPuppet(looks)
         .merge({ speed: vnew(), isOnGround: true }) // TODO probably from some physics engine base
         .step(() => {
-            const leftDown = Input.isDown('MoveLeft');
-            const rightDown = Input.isDown('MoveRight');
+            const moveLeft = Input.isDown('MoveLeft');
+            const moveRight = Input.isDown('MoveRight');
+            const duck = Input.isDown('Duck');
 
-            if ((leftDown && rightDown) || (!leftDown && !rightDown)) {
+            // TODO probably expose so that attackers can see this?
+            const isDucking = duck && puppet.isOnGround;
+
+            // TODO probably things beyond here should be abstracted into "objLocomotiveIguana"
+            if ((moveLeft && moveRight) || (!moveLeft && !moveRight) || isDucking) {
                 puppet.speed.x = approachLinear(puppet.speed.x, 0, PlayerConsts.WalkingDeceleration);
             }
-            else if (leftDown)
+            else if (moveLeft)
                 puppet.speed.x = Math.max(puppet.speed.x - PlayerConsts.WalkingDeceleration, -PlayerConsts.WalkingTopSpeed);
-            else if (rightDown)
+            else if (moveRight)
                 puppet.speed.x = Math.min(puppet.speed.x + PlayerConsts.WalkingDeceleration, PlayerConsts.WalkingTopSpeed);
 
             if (puppet.speed.x !== 0) {
@@ -43,7 +48,9 @@ export function objPlayer(looks: IguanaLooks.Serializable) {
             puppet.facing = approachLinear(
                 puppet.facing,
                 Math.sign(puppet.speed.x) || lastNonZeroSpeedXSign || Math.sign(puppet.facing),
-                Math.max(Math.min(Math.abs(puppet.speed.x * 0.05), 0.5), 0.1))
+                Math.max(Math.min(Math.abs(puppet.speed.x * 0.05), 0.5), 0.1));
+
+            puppet.ducking = approachLinear(puppet.ducking, isDucking ? 1 : 0, 0.075);
         })
         .step(() => {
             puppet.add(puppet.speed);
