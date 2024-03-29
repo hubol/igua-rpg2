7b0df636641b9186c67bfefde03e378e1be602ab
Hubol <hubol.gordon@gmail.com>
2024-03-29 17:10:50 -0500

    Port DexiePromise changes https://github.com/dexie/Dexie.js/commit/3f27079963d5e6a034ce8789732e34f49c8f28d3 https://github.com/dexie/Dexie.js/commit/09058eab1563d3b4eaee9c9dd3a9a03705bac132

--- src/lib/zone/dexie/promise.ts
+++ src/lib/zone/dexie/promise.ts
@@ -504,12 +504,10 @@ props(DexiePromise.prototype, {
   finally: function (onFinally) {
     return this.then(
       (value) => {
-        onFinally();
-        return value;
+        return DexiePromise.resolve(onFinally()).then(()=>value);
       },
       (err) => {
-        onFinally();
-        return PromiseReject(err);
+        return DexiePromise.resolve(onFinally()).then(()=>PromiseReject(err));
       }
     );
   },
@@ -993,8 +991,6 @@ export function newScope(fn, props, a1?, a2?) {
         any: (DexiePromise as unknown as Es2020Promise).any,
         resolve: DexiePromise.resolve,
         reject: DexiePromise.reject,
-        nthen: getPatchedPromiseThen(globalEnv.nthen, psd), // native then
-        gthen: getPatchedPromiseThen(globalEnv.gthen, psd), // global then
       }
     : {};
   if (props) extend(psd, props);
@@ -1103,11 +1099,6 @@ function switchToZone(targetZone, bEnteringZone) {
     // Swich environments (may be PSD-zone or the global zone. Both apply.)
     var targetEnv = targetZone.env;
 
-    // Change Promise.prototype.then for native and global Promise (they MAY differ on polyfilled environments, but both can be accessed)
-    // Must be done on each zone change because the patched method contains targetZone in its closure.
-    nativePromiseProto.then = targetEnv.nthen;
-    GlobalPromise.prototype.then = targetEnv.gthen;
-
     if (currentZone.global || targetZone.global) {
       // Leaving or entering global zone. It's time to patch / restore global Promise.
 
@@ -1140,8 +1131,6 @@ function snapShot() {
         any: GlobalPromise.any,
         resolve: GlobalPromise.resolve,
         reject: GlobalPromise.reject,
-        nthen: nativePromiseProto.then,
-        gthen: GlobalPromise.prototype.then,
       }
     : {};
 }
