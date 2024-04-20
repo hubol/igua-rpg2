87ef42cb4f69df284d457ecdb7fbbd6eb8550ffe
Hubol <hubol.gordon@gmail.com>
2024-04-19 20:31:19 -0500

    Utility type for getting mixin features

---
+++ src/lib/game-engine/mixin-features.ts
@@ -0,0 +1,6 @@
+type TypeError_MixinFunctionResultTypeDoesNotExtendFirstParameterType = 'Mixin function result type does not extend first parameter type';
+type TypeError_DoesNotAppearToBeAMixinFunction = 'Does not appear to be a mixin function';
+
+export type MixinFeatures<TFn extends (...args: any[]) => any> = TFn extends (src: infer TMinimum, ...rest: any[]) => infer TDst
+    ? (TDst extends TMinimum ? Omit<TDst, keyof TMinimum> : TypeError_MixinFunctionResultTypeDoesNotExtendFirstParameterType)
+    : TypeError_DoesNotAppearToBeAMixinFunction;
