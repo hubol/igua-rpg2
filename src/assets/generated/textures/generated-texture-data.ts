fac10676b4f97ce249f6b18a4603b60d4c321b49
Hubol <hubol.gordon@gmail.com>
2024-05-14 16:53:24 -0500

    Gray pipe texture

--- src/assets/generated/textures/generated-texture-data.ts
+++ src/assets/generated/textures/generated-texture-data.ts
@@ -1,6 +1,6 @@
 // This file is generated
 
-const atlases = [{ url: require("./atlas0.png"), texturesCount: 25 }];
+const atlases = [{ url: require("./atlas0.png"), texturesCount: 26 }];
 
 interface TxData {
   id: string;
@@ -24,7 +24,7 @@ function txs<T>(tx: (data: TxData) => T) {
     Iguana: {
       Club: tx({ id: "Iguana.Club", atlas: 0, x: 760, y: 67, width: 48, height: 12 }),
       Crest: tx({ id: "Iguana.Crest", atlas: 0, x: 906, y: 31, width: 112, height: 16 }),
-      Eye: tx({ id: "Iguana.Eye", atlas: 0, x: 1011, y: 7, width: 8, height: 8 }),
+      Eye: tx({ id: "Iguana.Eye", atlas: 0, x: 383, y: 83, width: 8, height: 8 }),
       Foot: tx({ id: "Iguana.Foot", atlas: 0, x: 765, y: 54, width: 140, height: 12 }),
       Head: tx({ id: "Iguana.Head", atlas: 0, x: 992, y: 7, width: 18, height: 18 }),
       Horn: tx({ id: "Iguana.Horn", atlas: 0, x: 711, y: 60, width: 48, height: 8 }),
@@ -38,10 +38,15 @@ function txs<T>(tx: (data: TxData) => T) {
     OpenDoor: tx({ id: "OpenDoor", atlas: 0, x: 858, y: 67, width: 30, height: 32 }),
     OversizedAngel: tx({ id: "OversizedAngel", atlas: 0, x: 0, y: 31, width: 396, height: 51 }),
     Placeholder: tx({ id: "Placeholder", atlas: 0, x: 1009, y: 48, width: 14, height: 14 }),
+    Terrain: {
+      Pipe: {
+        Gray: tx({ id: "Terrain.Pipe.Gray", atlas: 0, x: 1023, y: 0, width: 1, height: 9 }),
+      },
+    },
     Ui: {
       Checkbox: tx({ id: "Ui.Checkbox", atlas: 0, x: 931, y: 0, width: 60, height: 30 }),
       ChooseYourLooksIcons: tx({ id: "Ui.ChooseYourLooksIcons", atlas: 0, x: 0, y: 0, width: 930, height: 30 }),
-      NoneChoice: tx({ id: "Ui.NoneChoice", atlas: 0, x: 383, y: 83, width: 8, height: 8 }),
+      NoneChoice: tx({ id: "Ui.NoneChoice", atlas: 0, x: 1011, y: 7, width: 8, height: 8 }),
       PlacementReticle: tx({ id: "Ui.PlacementReticle", atlas: 0, x: 1011, y: 16, width: 6, height: 6 }),
     },
   };
