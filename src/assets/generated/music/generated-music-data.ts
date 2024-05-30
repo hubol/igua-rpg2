4be08cb4147873e5cc9708066ba7c2b78883e45d
Hubol <hubol.gordon@gmail.com>
2024-05-29 20:39:28 -0500

    Re-run smooch with consistent order

--- src/assets/generated/music/generated-music-data.ts
+++ src/assets/generated/music/generated-music-data.ts
@@ -2,6 +2,6 @@
 // This file is generated
 
 export const GeneratedMusicData = {
-   "Test2": { ogg: require("./ogg/test2.ogg") },
-   "Test1": { ogg: require("./ogg/test1.ogg") }
+   "Test1": { ogg: require("./ogg/test1.ogg") },
+   "Test2": { ogg: require("./ogg/test2.ogg") }
 }
--- src/assets/generated/sounds/generated-sfx-data.ts
+++ src/assets/generated/sounds/generated-sfx-data.ts
@@ -3,36 +3,36 @@
 async function sfxs<T>(sfx: (ogg: string) => Promise<T>) {
   const sounds = await Promise.all(
     [
-      "pork roll egg and cheese.ogg",
-      "beep tiny.ogg",
-      "ballon pop.ogg",
-      "ball bounce.ogg",
-      "arrow knock.ogg",
       "activate lever.ogg",
-      "ui/select.ogg",
-      "ui/navigate into.ogg",
-      "ui/navigate back.ogg",
+      "arrow knock.ogg",
+      "ball bounce.ogg",
+      "ballon pop.ogg",
+      "beep tiny.ogg",
       "folder/cast spell hit.ogg",
+      "pork roll egg and cheese.ogg",
       "ui/looks/updated.ogg",
+      "ui/navigate back.ogg",
+      "ui/navigate into.ogg",
+      "ui/select.ogg",
     ].map(sfx),
   );
   return {
-    PorkRollEggAndCheese: sounds[0],
-    BeepTiny: sounds[1],
-    BallonPop: sounds[2],
-    BallBounce: sounds[3],
-    ArrowKnock: sounds[4],
-    ActivateLever: sounds[5],
-    Ui: {
-      Select: sounds[6],
-      NavigateInto: sounds[7],
-      NavigateBack: sounds[8],
-      Looks: {
-        Updated: sounds[10],
-      },
-    },
+    ActivateLever: sounds[0],
+    ArrowKnock: sounds[1],
+    BallBounce: sounds[2],
+    BallonPop: sounds[3],
+    BeepTiny: sounds[4],
     Folder: {
-      CastSpellHit: sounds[9],
+      CastSpellHit: sounds[5],
+    },
+    PorkRollEggAndCheese: sounds[6],
+    Ui: {
+      Looks: {
+        Updated: sounds[7],
+      },
+      NavigateBack: sounds[8],
+      NavigateInto: sounds[9],
+      Select: sounds[10],
     },
   };
 }
