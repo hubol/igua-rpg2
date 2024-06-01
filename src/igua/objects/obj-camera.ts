26947dc0ecc5ad7ef3be798edcb7f5b4584c4a0f
Hubol <hubol.gordon@gmail.com>
2024-06-01 16:14:30 -0500

    Always snap to player

--- src/igua/objects/obj-camera.ts
+++ src/igua/objects/obj-camera.ts
@@ -1,10 +1,6 @@
-import { moveTowards } from "../../lib/math/vector";
-import { vnew } from "../../lib/math/vector-type";
 import { container } from "../../lib/pixi/container";
 import { renderer, scene } from "../globals";
 import { playerObj } from "./obj-player";
-
-const v = vnew();
 
 export function objCamera() {
     // TODO need way to snap to desired position e.g. on level load
@@ -12,7 +8,7 @@ export function objCamera() {
         // TODO camera behavior should be overrideable
         // e.g. there should be modes other than following the player
         if (!playerObj?.destroyed)
-            self.moveTowards(v.at(playerObj).add(-128, -128), 4);
+            self.at(playerObj).add(-128, -128);
 
         // TODO switch for this?
         self.x = Math.max(0, Math.min(self.x, scene.level.width - renderer.width));
