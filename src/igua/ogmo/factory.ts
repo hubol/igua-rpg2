6bcc910a4e25832a7a7c2a40fccc3b4624ec687d
Hubol <hubol.gordon@gmail.com>
2024-07-04 18:22:06 -0500

    Reorganize OgmoFactory namespace

--- src/igua/ogmo/factory.ts
+++ src/igua/ogmo/factory.ts
@@ -1,7 +1,7 @@
 import { Sprite, Texture } from "pixi.js";
 import { scene } from "../globals";
 
-namespace OgmoFactory {
+export namespace OgmoFactory {
     export interface Entity {
         x: number;
         y: number;
@@ -10,7 +10,7 @@ namespace OgmoFactory {
         width?: number;
         height?: number;
         tint?: number;
-        values: EntityValues & Record<string, string | number>;
+        values: EntityValues & Record<string, any>;
     }    
 
     interface EntityValues {
@@ -34,56 +34,50 @@ namespace OgmoFactory {
         height: number;
         backgroundTint: number;
     }
-}
 
-function createEntity<TFn extends (...args: any[]) => any>(fn: TFn, entity: OgmoFactory.Entity): ReturnType<TFn> {
-    const obj: Sprite = fn(entity);
-    obj.add(entity);
-
-    if (entity.width !== undefined)
-        obj.scale.x = entity.width;
-
-    if (entity.height !== undefined)
-        obj.scale.y = entity.height;
-
-    if (entity.flippedX)
-        obj.scale.x *= -1;
-
-    if (entity.flippedY)
-        obj.scale.y *= -1;
-
-    if (entity.tint !== undefined)
-        obj.tint = entity.tint;
-
-    if (!obj.parent)
-        obj.show();
-
-    return obj as any;
-}
-
-function createDecal(texture: Texture, decal: OgmoFactory.Decal) {
-    const spr = Sprite.from(texture).at(decal.x, decal.y);
-    spr.scale.set(decal.scaleX, decal.scaleY);
-    spr.angle = decal.rotation;
-    spr.anchor.set(decal.originX, decal.originY);
-
-    if (decal.tint !== undefined)
-        spr.tint = decal.tint;
-
-    return spr.show();
-}
-
-function createLevel<TFn extends (...args: any[]) => any>(level: OgmoFactory.Level, fn: TFn): TFn {
-    return (() => {
-        scene.level.width = level.width;
-        scene.level.height = level.height;
-        scene.style.backgroundTint = level.backgroundTint;
-        return fn();
-    }) as TFn;
-}
-
-export const OgmoFactory = {
-    createEntity,
-    createDecal,
-    createLevel,
+    export function createEntity<TFn extends (...args: any[]) => any>(fn: TFn, entity: OgmoFactory.Entity): ReturnType<TFn> {
+        const obj: Sprite = fn(entity);
+        obj.add(entity);
+    
+        if (entity.width !== undefined)
+            obj.scale.x = entity.width;
+    
+        if (entity.height !== undefined)
+            obj.scale.y = entity.height;
+    
+        if (entity.flippedX)
+            obj.scale.x *= -1;
+    
+        if (entity.flippedY)
+            obj.scale.y *= -1;
+    
+        if (entity.tint !== undefined)
+            obj.tint = entity.tint;
+    
+        if (!obj.parent)
+            obj.show();
+    
+        return obj as any;
+    }
+    
+    export function createDecal(texture: Texture, decal: OgmoFactory.Decal) {
+        const spr = Sprite.from(texture).at(decal.x, decal.y);
+        spr.scale.set(decal.scaleX, decal.scaleY);
+        spr.angle = decal.rotation;
+        spr.anchor.set(decal.originX, decal.originY);
+    
+        if (decal.tint !== undefined)
+            spr.tint = decal.tint;
+    
+        return spr.show();
+    }
+    
+    export function createLevel<TFn extends (...args: any[]) => any>(level: OgmoFactory.Level, fn: TFn): TFn {
+        return (() => {
+            scene.level.width = level.width;
+            scene.level.height = level.height;
+            scene.style.backgroundTint = level.backgroundTint;
+            return fn();
+        }) as TFn;
+    }
 }
