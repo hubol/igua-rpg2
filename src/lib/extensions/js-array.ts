20b60ac322161a2fad9e63314f4a4a2f8f1c6979
Hubol <hubol.gordon@gmail.com>
2024-07-12 17:59:15 -0500

    Array.removeFirst

--- src/lib/extensions/js-array.ts
+++ src/lib/extensions/js-array.ts
@@ -1,19 +1,28 @@
 declare global {
     interface Array<T> {
         last: T;
+        removeFirst(value: T): void;
     }
 }
 
 Object.defineProperties(Array.prototype, {
     last: {
-        get: function () {
+        get: function <T>(this: Array<T>) {
             return this[this.length - 1];
         },
-        set: function (value) {
+        set: function <T>(value: T) {
             this[this.length - 1] = value;
         },
         configurable: true,
     },
+    removeFirst: {
+        value: function <T>(this: Array<T>, value: T) {
+            const index = this.indexOf(value);
+            if (index > -1)
+                this.splice(index, 1);
+        },
+        configurable: true,
+    }
 });
 
 export default 0;
