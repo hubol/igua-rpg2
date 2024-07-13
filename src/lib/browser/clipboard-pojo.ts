c413e7c50bb04b0e8fde19d09f47a9333f424a39
Hubol <hubol.gordon@gmail.com>
2024-07-12 22:40:18 -0500

    Suppress esbuild warning re: eval

--- src/lib/browser/clipboard-pojo.ts
+++ src/lib/browser/clipboard-pojo.ts
@@ -4,7 +4,7 @@ export const ClipboardPojo = {
     async read<T = Record<string, any>>(): Promise<T> {
         const text = await navigator.clipboard.readText();
         try {
-            const result = eval('const __result__ = ' + text + '; __result__;');
+            const result = (0, eval)('const __result__ = ' + text + '; __result__;');
             if (typeof result !== 'object')
                 throw new Error('Clipboard text did not evaluate to a JavaScript object');
             return result;
