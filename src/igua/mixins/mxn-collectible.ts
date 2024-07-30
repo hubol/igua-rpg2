81b06cbe5c77e3a0c72ac224d6bf7d620e8c5f24
Hubol <hubol.gordon@gmail.com>
2024-07-29 20:48:35 -0500

    Overengineered collectible mixin

---
+++ src/igua/mixins/mxn-collectible.ts
@@ -0,0 +1,60 @@
+import { DisplayObject } from "pixi.js";
+import { playerObj } from "../objects/obj-player";
+import { RpgProgress, RpgProgressFlags, RpgProgressUids } from "../rpg/rpg-progress";
+import { DeepAccess } from "../../lib/object/deep-access";
+
+interface MxnCollectibleUid {
+    type: 'uid';
+    uid: number;
+    set: RpgProgressUids;
+}
+
+interface MxnCollectibleFlag {
+    type: 'flag';
+    flag: RpgProgressFlags;
+}
+
+interface MxnCollectibleTransient {
+    type: 'transient';
+}
+
+type MxnCollectibleArgs = MxnCollectibleUid | MxnCollectibleFlag | MxnCollectibleTransient;
+
+function isCollected(args: MxnCollectibleArgs): boolean {
+    if (args.type === 'transient')
+        return false;
+    if (args.type === 'flag')
+        return DeepAccess.get(RpgProgress.flags, args.flag);
+    return RpgProgress.uids[args.set].has(args.uid);
+}
+
+function collect(args: MxnCollectibleArgs) {
+    if (args.type === 'transient')
+        return;
+    if (args.type === 'flag')
+        DeepAccess.set(RpgProgress.flags, args.flag, true);
+    else
+        RpgProgress.uids[args.set].add(args.uid);
+}
+
+export function mxnCollectible(obj: DisplayObject, args: MxnCollectibleArgs) {
+    const initialCollected = isCollected(args);
+
+    const collectible = obj
+    .dispatches<'collected'>()
+    .step(() => {
+            if (initialCollected)
+                return obj.destroy();
+
+            if (playerObj.hasControl && playerObj.collides(obj)) {
+                collect(args);
+                collectible.dispatch('collected');
+                obj.destroy();
+            }
+        })
+
+    if (initialCollected)
+        obj.visible = false;
+
+    return collectible;
+}
--- src/igua/objects/obj-valuable.ts
+++ src/igua/objects/obj-valuable.ts
@@ -1,10 +1,10 @@
 import { Sprite, Texture } from "pixi.js";
 import { Tx } from "../../assets/textures";
-import { playerObj } from "./obj-player";
 import { RpgProgress } from "../rpg/rpg-progress";
 import { VectorSimple, vnew } from "../../lib/math/vector-type";
 import { objValuableSparkle } from "./effects/obj-valuable-sparkle";
 import { Rng } from "../../lib/math/rng";
+import { mxnCollectible } from "../mixins/mxn-collectible";
 
 type ValuableType = 'green' | 'orange' | 'blue';
 
@@ -42,21 +42,19 @@ const valuableConfigs: Record<ValuableType, ValuableConfig> = {
 }
 
 // TODO flag id argument for valuables placed in levels
-export function objValuable(type: ValuableType) {
+export function objValuable(type: ValuableType, uid?: number) {
     const config = valuableConfigs[type];
     return Sprite.from(config.texture)
         .anchored(0.5, 0.5)
-        .step(self => {
-            if (self.collides(playerObj)) {
-                // TODO sfx
-                // TODO vfx
-                for (const offset of config.sparkle.offsets) {
-                    const sparkle = objValuableSparkle()
-                    .at(self).add(offset).show(self.parent);
-                    sparkle.index = Rng.float(1, 3);
-                }
-                RpgProgress.character.valuables += config.value;
-                self.destroy();
+        .mixin(mxnCollectible, uid === undefined ? { type: 'transient' } : { type: 'uid', uid, set: 'valuables' })
+        .handles('collected', self => {
+            // TODO sfx
+            // TODO vfx
+            for (const offset of config.sparkle.offsets) {
+                const sparkle = objValuableSparkle()
+                .at(self).add(offset).show(self.parent);
+                sparkle.index = Rng.float(1, 3);
             }
+            RpgProgress.character.valuables += config.value;
         })
 }
