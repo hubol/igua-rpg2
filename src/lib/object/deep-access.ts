eef78a69289281a1b3bee359d2a08599ef673fd0
Hubol <hubol.gordon@gmail.com>
2024-07-29 20:45:08 -0500

    Add deep access

---
+++ src/lib/object/deep-access.ts
@@ -0,0 +1,13 @@
+export const DeepAccess = {
+    get<T extends Record<string, any>>(object: T, key: string): any {
+        return key.split('.').reduce((memo, part) => memo[part], object);
+    },
+    set<T extends Record<string, any>>(object: T, key: string, value: any) {
+        key.split('.').reduce((memo, part, index, array) => {
+            if (index === array.length - 1)
+                (memo[part] as any) = value;
+            else
+                return memo[part];
+        }, object)
+    },
+}
