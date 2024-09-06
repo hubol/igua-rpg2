68901a9d6cd271a4e1a268658216d1caaca2966c
Hubol <hubol.gordon@gmail.com>
2024-09-06 16:30:01 -0500

    Simple Coro.all

--- src/lib/game-engine/routines/coro.ts
+++ src/lib/game-engine/routines/coro.ts
@@ -4,6 +4,7 @@ export namespace Coro {
     export type Predicate = () => boolean;
     export type Type<T = unknown> = Generator<Predicate, T, unknown>;
 
+    // TODO return value could go here!!
     interface RunnerState {
         predicate: Predicate | null;
         done: boolean;
@@ -11,14 +12,14 @@ export namespace Coro {
 
     export const runner = (generator: Type, state: RunnerState) => {
         if (state.done)
-            return;
+            return true;
 
         for (let i = 0; i < 256; i++) {
             if (state.predicate === null) {
                 const next = generator.next();
                 if (next.done) {
                     state.done = true;
-                    return;
+                    return true;
                 }
                 
                 state.predicate = next.value;
@@ -27,9 +28,51 @@ export namespace Coro {
             if (state.predicate())
                 state.predicate = null;
             else
-                return;
+                return false;
         }
 
         ErrorReporter.reportDevOnlyState(new Error(`Possible infinite coro loop detected!`), generator, state.predicate);
+        return false;
+    }
+
+    // TODO name
+    type ReturnedByGenerator<T> = T extends Coro.Type<infer U> ? U : never
+
+    export function* all<T extends readonly (Coro.Type | Predicate)[] | []>(values: T): Coro.Type<{ -readonly [P in keyof T]: ReturnedByGenerator<T[P]> }> {
+        const completedIndices: boolean[] = [];
+        const results: any[] = [];
+        const predicates: Predicate[] = [];
+
+        const length = values.length;
+        let toCompleteCount = length;
+
+        for (let i = 0; i < length; i++) {
+            const value = values[i];
+            const predicate = isCoroType(value)
+                ? runner.bind(null, value, { predicate: null, done: false })
+                : value;
+
+            predicates.push(predicate);
+        }
+
+        yield () => {
+            for (let i = 0; i < length; i++) {
+                if (completedIndices[i])
+                    continue;
+                if (predicates[i]()) {
+                    // TODO add to results
+                    completedIndices[i] = true;
+                    toCompleteCount--;
+                }
+            }
+            return toCompleteCount <= 0;
+        }
+
+        // TODO
+        return results as any;
+    }
+
+    function isCoroType(value: Coro.Type | Predicate): value is Coro.Type {
+        return value['next'];
     }
 }
