6dd9814271898e4ec44e14fa28abfb2819143747
Hubol <hubol.gordon@gmail.com>
2024-10-28 21:45:24 -0500

    Cutened sign

--- src/igua/objects/obj-sign.ts
+++ src/igua/objects/obj-sign.ts
@@ -13,12 +13,12 @@ interface ObjSignArgs {
 
 export function objSign({ title, message }: ObjSignArgs) {
     const spr = Sprite.from(Tx.Wood.Sign);
-    const text = objText.Small(title, { tint: 0x896037 }).at(1, 1);
+    const text = objText.Small(title, { tint: 0x854E31 }).anchored(0.5, 0.5).at(25, 11);
 
     return container(spr, text)
         .mixin(mxnCutscene, () => {
             Sfx.Interact.SignRead.play();
             return show(message);
         })
-        .at(0, -16);
+        .at(0, -23);
 }
