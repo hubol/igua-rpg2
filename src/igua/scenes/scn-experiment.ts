4b3a1be4a5ed5976c2f58072fdd0141237d4fbeb
Hubol <hubol.gordon@gmail.com>
2024-11-06 20:26:53 -0600

    Play some music, pick starting scenes for better impression

--- src/igua/scenes/scn-experiment.ts
+++ src/igua/scenes/scn-experiment.ts
@@ -1,5 +1,8 @@
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { Mzk } from "../../assets/music";
+import { Jukebox } from "../core/igua-audio";
 
 export function scnExperiment() {
+    Jukebox.play(Mzk.TrashDay);
     Lvl.Experiment();
 }
