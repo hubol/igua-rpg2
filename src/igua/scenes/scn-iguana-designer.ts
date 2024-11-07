cb8ebbeed7aa627a8daa83122f12c70529b8af9e
Hubol <hubol.gordon@gmail.com>
2024-11-06 23:40:06 -0600

    More ridiculous effects

--- src/igua/scenes/scn-iguana-designer.ts
+++ src/igua/scenes/scn-iguana-designer.ts
@@ -1,3 +1,4 @@
+import { DisplayObject } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
 import { Mzk } from "../../assets/music";
 import { sleepf } from "../../lib/game-engine/routines/sleep";
@@ -6,6 +7,7 @@ import { container } from "../../lib/pixi/container";
 import { Jukebox } from "../core/igua-audio";
 import { mxnBoilPivot } from "../mixins/mxn-boil-pivot";
 import { CtxUiIguanaDesigner, objUiIguanaDesignerRoot } from "../ui/iguana-designer/obj-ui-iguana-designer-root";
+import { approachLinear } from "../../lib/math/number";
 
 export function scnIguanaDesigner() {
     Jukebox.play(Mzk.FirstSong);
@@ -17,7 +19,8 @@ export function scnIguanaDesigner() {
     const objs = new PseudoRng(69).shuffle([
         LightShadowIrregularSmall,
         ...Group1.children,
-    ]);
+    ])
+        .map(obj => obj.mixin(mxnFliesAway));
 
     // TODO should there be a "fiber" abstraction
     // so that you don't have to write container().coro().show() ?
@@ -25,14 +28,14 @@ export function scnIguanaDesigner() {
         while (true) {
             yield () => CtxUiIguanaDesigner.value.isViewingConfirmPage;
             for (let i = 0; i < objs.length; i++) {
-                objs[i].visible = false;
+                objs[i].isFlyingAway = true;
                 if (i % 3 !== 2) {
                     yield sleepf(1);
                 }
             }
             yield () => !CtxUiIguanaDesigner.value.isViewingConfirmPage;
             for (let i = objs.length - 1; i >= 0; i--) {
-                objs[i].visible = true;
+                objs[i].isFlyingAway = false;
                 if (i % 3 !== 2) {
                     yield sleepf(1);
                 }
@@ -40,4 +43,15 @@ export function scnIguanaDesigner() {
         }
     })
         .show();
+}
+
+function mxnFliesAway(obj: DisplayObject) {
+    const y = obj.y;
+    let virtualY = y;
+
+    return obj.merge({ isFlyingAway: false })
+        .step(self => {
+            virtualY = approachLinear(virtualY, self.isFlyingAway ? -64 : y, 12);
+            self.y = virtualY;
+        });
 }
