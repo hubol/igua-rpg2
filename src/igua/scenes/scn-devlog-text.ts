4cc9bce3854ff796e83cbd3cc1902eb6fa5e38ab
Hubol <hubol.gordon@gmail.com>
2024-11-08 23:43:57 -0600

    Set seed in devlog text scene

--- src/igua/scenes/scn-devlog-text.ts
+++ src/igua/scenes/scn-devlog-text.ts
@@ -8,12 +8,21 @@ export function scnDevlogText() {
     scene.style.backgroundTint = 0x808080;
     const c = container().at(0, 96).show();
 
-    const title = "Collectable-once items, OGMO unique IDs";
-    const date = "29 July 2024";
+    const title = "More style, iguana editor changes";
+    const date = "4 November 2024";
+
+    const seed1 = title.charCodeAt(1) * 1357;
+    const seed2 = date.charCodeAt(1) * 1234;
 
     const bg1 = objText.LargeBold(title, { tint: 0x000000 }).scaled(4, 4).at(32, 880).show(c);
     const bg2 = objText.Large(date, { tint: 0x000000 }).scaled(3, 3).at(32, 930).show(c);
 
-    objText.LargeBold(title).scaled(bg1.scale).at(bg1.vcpy().add(bg1.scale, -1)).show(c);
-    objText.Large(date).scaled(bg2.scale).at(bg2.vcpy().add(bg2.scale, -1)).show(c);
+    const text1 = objText.LargeBold(title).scaled(bg1.scale).at(bg1.vcpy().add(bg1.scale, -1)).show(c);
+    const text2 = objText.Large(date).scaled(bg2.scale).at(bg2.vcpy().add(bg2.scale, -1)).show(c);
+
+    bg1.seed = seed1;
+    text1.seed = seed1;
+
+    bg2.seed = seed2;
+    text2.seed = seed2;
 }
