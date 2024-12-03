7afbf1d4df52971d3e987f86278b1b0fce18ed14
Hubol <hubol.gordon@gmail.com>
2024-12-02 21:56:23 -0600

    Don't let loading screen spin at all in dev

--- src/igua/launch/show-loading-screen.ts
+++ src/igua/launch/show-loading-screen.ts
@@ -1,25 +1,29 @@
 import { JobProgress } from "../../lib/game-engine/job-progress";
-import { Environment } from "../../lib/environment";
 import { approachLinear } from "../../lib/math/number";
 import { timeoutSleep } from "../../lib/browser/timeout-sleep";
+import { Environment } from "../../lib/environment";
 
 export async function showLoadingScreen(progress: JobProgress) {
+    if (Environment.isProduction) {
+        await showAnimatedLoadingScreen(progress);
+    }
+
+    dom.loadingEl.remove();
+}
+
+async function showAnimatedLoadingScreen(progress: JobProgress) {
     let unit = 0;
-    const shouldResolve = createShouldResolve(progress);
 
-    const preloadEl = document.querySelector<HTMLElement>("#preload")!;
-    preloadEl.style.opacity = "0";
-
-    const el = document.querySelector<HTMLElement>("#loading_bar .front")!;
+    dom.preloadEl.style.opacity = "0";
 
     await new Promise<void>(resolve => {
         const render = () => {
-            if (shouldResolve(unit)) {
+            if (progress.completed && unit >= 1) {
                 return resolve();
             }
 
             unit = approachLinear(unit, progress.percentage, 1 / 60);
-            el.style.width = `${unit * 100}%`;
+            dom.loadingBarEl.style.width = `${unit * 100}%`;
 
             requestAnimationFrame(render);
         };
@@ -27,20 +31,18 @@ export async function showLoadingScreen(progress: JobProgress) {
         render();
     });
 
-    const loadingEl = document.querySelector<HTMLElement>("#loading")!;
-
-    if (!Environment.isDev) {
-        loadingEl.classList.add("hide");
-        await timeoutSleep(200);
-    }
-
-    loadingEl.remove();
+    dom.loadingEl.classList.add("hide");
+    await timeoutSleep(200);
 }
 
-function createShouldResolve(progress: JobProgress) {
-    if (Environment.isDev) {
-        return (unit: number) => progress.completed;
-    }
-
-    return (unit: number) => progress.completed && unit >= 1;
-}
+const dom = {
+    get preloadEl() {
+        return document.querySelector<HTMLElement>("#preload")!;
+    },
+    get loadingEl() {
+        return document.querySelector<HTMLElement>("#loading")!;
+    },
+    get loadingBarEl() {
+        return document.querySelector<HTMLElement>("#loading_bar .front")!;
+    },
+};
