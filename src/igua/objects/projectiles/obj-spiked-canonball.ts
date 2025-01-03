72f391ce4f2bcaee4ed8be5dc5897cadc5d73a04
Hubol <hubol.gordon@gmail.com>
2025-01-03 00:56:52 -0600

    Randomized burst tints for canonball

--- src/igua/objects/projectiles/obj-spiked-canonball.ts
+++ src/igua/objects/projectiles/obj-spiked-canonball.ts
@@ -5,8 +5,9 @@ import { CollisionShape } from "../../../lib/pixi/collision";
 import { mxnSpatialAudio } from "../../mixins/mxn-spatial-audio";
 import { Sfx } from "../../../assets/sounds";
 import { Rng } from "../../../lib/math/rng";
-import { objFxBurstRound24 } from "../effects/obj-fx-burst-round-24";
 import { objFxBurst32 } from "../effects/obj-fx-burst-32";
+
+const burstTints = [0x845FD3, 0xBEC71D, 0x5DE478, 0xBC362F];
 
 export function objSpikedCanonball() {
     return Sprite.from(Tx.Enemy.SpikeBall)
@@ -19,7 +20,7 @@ export function objSpikedCanonball() {
             yield () => self.speed.y > 0;
             const speedX = self.speed.x;
             yield () => self.speed.y === 0;
-            objFxBurst32().at(self).add(0, 8).show(self.parent);
+            objFxBurst32().at(self).add(0, 8).tinted(Rng.choose(...burstTints)).show(self.parent);
             self.play(Sfx.Impact.SpikedCanonballLand.with.rate(Rng.float(0.9, 1.1)));
             self.speed.y = -2;
             self.speed.x = speedX * 0.3;
