b4b562945bfa80077eff53903fb06ea7f45d197b
Hubol <hubol.gordon@gmail.com>
2025-01-17 18:04:04 -0600

    Fishmonger speaks

--- src/igua/scenes/scn-new-balltown-fishmonger.ts
+++ src/igua/scenes/scn-new-balltown-fishmonger.ts
@@ -1,13 +1,16 @@
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
 import { Mzk } from "../../assets/music";
 import { Jukebox } from "../core/igua-audio";
+import { ask, show } from "../cutscene/show";
 import { mxnBoilPivot } from "../mixins/mxn-boil-pivot";
+import { mxnCutscene } from "../mixins/mxn-cutscene";
 import { objFish } from "../objects/obj-fish";
 
 export function scnNewBalltownFishmonger() {
     Jukebox.play(Mzk.BreadCrumbPool);
     const lvl = Lvl.NewBalltownFishmonger();
     enrichAquarium(lvl);
+    enrichFishmonger(lvl);
 }
 
 function enrichAquarium(lvl: ReturnType<typeof Lvl["NewBalltownFishmonger"]>) {
@@ -16,4 +19,25 @@ function enrichAquarium(lvl: ReturnType<typeof Lvl["NewBalltownFishmonger"]>) {
     }
 
     lvl.WaterLineTop.mixin(mxnBoilPivot);
+}
+
+function enrichFishmonger(lvl: ReturnType<typeof Lvl["NewBalltownFishmonger"]>) {
+    lvl.Fishmonger.mixin(mxnCutscene, function* () {
+        yield* show("Hello! I deal in fish.");
+        const result = yield* ask("Can I help you somehow?", "Favorite fish aspect", "Fish delivery", "No");
+        if (result === 0) {
+            yield* show(
+                "Fish are very entertaining to watch.",
+                "I feel bad that I have trapped them in a jail cell. But boy are they cute!",
+                "I want to share my fish with the world!",
+            );
+        }
+        else if (result === 1) {
+            yield* ask("You want a fish delivered? That's great! Who wants a fish?", "I don't know");
+            yield* show("Oh, okay. Let me know if you find an interested party!");
+        }
+        else if (result === 2) {
+            yield* show("Okay! See you around!");
+        }
+    });
 }
