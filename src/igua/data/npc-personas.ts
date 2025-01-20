67a261231e702d9452bfab756103a03e4af5843f
hubol <hubol.gordon@gmail.com>
2025-01-19 21:26:10 -0600

    Very WIP croupier

--- src/igua/data/npc-personas.ts
+++ src/igua/data/npc-personas.ts
@@ -2,16 +2,18 @@ import { IguanaLooks } from "../iguana/looks";
 import { NpcLooks } from "./npc-looks";
 
 interface NpcPersona {
+    job: string;
     name: string;
     looks: IguanaLooks.Serializable;
 }
 
 export const NpcPersonas = {
-    BalltownOutskirtsMiner: { name: "Dante", looks: NpcLooks.Miner },
-    BalltownOutskirtsFarmer: { name: "Lars", looks: NpcLooks.Farmer },
-    NewBalltownBallFruitFanatic: { name: "Marf", looks: NpcLooks.BallFruitFanatic },
-    NewBalltownArmorer: { name: "Trav", looks: NpcLooks.Dizzy },
-    NewBalltownFishmonger: { name: "Pop", looks: NpcLooks.Nerd },
-    NewBalltownOliveFanatic: { name: "Oly", looks: NpcLooks.LivingOlive },
-    __Unknown__: { name: "???", looks: NpcLooks.MintyJourney },
+    BalltownOutskirtsMiner: { job: "Miner", name: "Dante", looks: NpcLooks.Miner },
+    BalltownOutskirtsFarmer: { job: "Farmer", name: "Lars", looks: NpcLooks.Farmer },
+    NewBalltownBallFruitFanatic: { job: "Eccentric", name: "Marf", looks: NpcLooks.BallFruitFanatic },
+    NewBalltownArmorer: { job: "Armorer", name: "Trav", looks: NpcLooks.Dizzy },
+    NewBalltownFishmonger: { job: "Fishmonger", name: "Pop", looks: NpcLooks.Nerd },
+    NewBalltownOliveFanatic: { job: "Autist", name: "Oly", looks: NpcLooks.LivingOlive },
+    NewBalltownCroupier: { job: "Croupier", name: "Flum", looks: NpcLooks.PartyAnimal },
+    __Unknown__: { job: "???", name: "???", looks: NpcLooks.MintyJourney },
 } satisfies Record<string, NpcPersona>;
--- src/igua/scenes/scn-new-balltown.ts
+++ src/igua/scenes/scn-new-balltown.ts
@@ -1,14 +1,19 @@
 import { Lvl, LvlType } from "../../assets/generated/levels/generated-level-data";
 import { Mzk } from "../../assets/music";
 import { sleep } from "../../lib/game-engine/routines/sleep";
+import { Rng } from "../../lib/math/rng";
+import { Empty } from "../../lib/types/empty";
 import { Jukebox } from "../core/igua-audio";
+import { rewardValuables } from "../cutscene/reward-valuables";
 import { ask, show } from "../cutscene/show";
 import { mxnCutscene } from "../mixins/mxn-cutscene";
+import { RpgProgress } from "../rpg/rpg-progress";
 
 export function scnNewBalltown() {
     Jukebox.play(Mzk.HomosexualFeet);
     const lvl = Lvl.NewBalltown();
     enrichOliveFanatic(lvl);
+    enrichCroupier(lvl);
 }
 
 function enrichOliveFanatic(lvl: LvlType.NewBalltown) {
@@ -42,4 +47,74 @@ function enrichOliveFanatic(lvl: LvlType.NewBalltown) {
     lvl.OliveFanatic.isDucking = true;
     lvl.OliveFanatic.ducking = 1;
     lvl.OliveFanatic.auto.duckingSpeed *= 0.5;
+}
+
+const risks = [1, 3, 5, 10, 25];
+
+function getAvailableRisks(currentValuables: number) {
+    const availableRisks = Empty<number>();
+    for (const risk of risks) {
+        if (currentValuables >= risk) {
+            availableRisks.push(risk);
+        }
+    }
+
+    return availableRisks;
+}
+
+function enrichCroupier(lvl: LvlType.NewBalltown) {
+    lvl.Croupier.mixin(mxnCutscene, function* () {
+        yield* show(
+            "The New Balltown committee recently ruled that gambling in the town square was OK depending on the vibes.",
+        );
+        if (yield* ask("Want to play a dice game?")) {
+            if (RpgProgress.character.inventory.valuables === 0) {
+                yield* show("Oh... you don't have any valuables...", "Let's play when you find some!");
+                return;
+            }
+            const guess = yield* ask("What face of the die will land face-up?", "1", "2", "3", "4", "5", "6");
+
+            const availableRisks = getAvailableRisks(RpgProgress.character.inventory.valuables);
+
+            const risked = yield* ask(
+                `How many valuables would you like to risk? You'll win five times as many if the die lands on ${
+                    guess + 1
+                }.`,
+                "Nevermind",
+                ...availableRisks.map(risk => `${risk}V`),
+            );
+
+            if (risked === 0) {
+                yield* show("Oh okay, maybe we can play later!");
+                return;
+            }
+
+            const riskedValue = availableRisks[risked - 1];
+
+            yield* show(`Okay. I'll take your ${riskedValue === 1 ? "valuable" : "valuables"} now.`);
+            // TODO sfx, vfx for take money
+            RpgProgress.character.inventory.valuables -= riskedValue;
+
+            yield sleep(500);
+
+            yield* show("Rolling the dice...");
+
+            yield sleep(500);
+
+            const roll = Rng.int(6);
+            yield* show(`It's a ${roll + 1}.`);
+
+            if (roll === guess) {
+                const prize = riskedValue * 5;
+                yield* show("Nice guess!", `You win ${prize} valuables!`);
+                yield* rewardValuables(prize, lvl.Croupier);
+            }
+            else {
+                yield* show("Sorry, you lost. You can always try again!");
+            }
+        }
+        else {
+            yield* show("I'll be here if you change your mind!");
+        }
+    });
 }
