19cb0c6b0c374c836f4623b275ed2a8749346469
Hubol <hubol.gordon@gmail.com>
2025-01-23 19:50:33 -0600

    Restructure NpcPersonas for internalName

--- src/igua/data/npc-personas.ts
+++ src/igua/data/npc-personas.ts
@@ -2,18 +2,26 @@ import { IguanaLooks } from "../iguana/looks";
 import { NpcLooks } from "./npc-looks";
 
 interface NpcPersona {
+    internalName: string;
     job: string;
     name: string;
     looks: IguanaLooks.Serializable;
 }
 
-export const NpcPersonas = {
-    BalltownOutskirtsMiner: { job: "Miner", name: "Dante", looks: NpcLooks.Miner },
-    BalltownOutskirtsFarmer: { job: "Farmer", name: "Lars", looks: NpcLooks.Farmer },
-    NewBalltownBallFruitFanatic: { job: "Eccentric", name: "Marf", looks: NpcLooks.BallFruitFanatic },
-    NewBalltownArmorer: { job: "Armorer", name: "Trav", looks: NpcLooks.Dizzy },
-    NewBalltownFishmonger: { job: "Fishmonger", name: "Pop", looks: NpcLooks.Nerd },
-    NewBalltownOliveFanatic: { job: "Autist", name: "Oly", looks: NpcLooks.LivingOlive },
-    NewBalltownCroupier: { job: "Croupier", name: "Flum", looks: NpcLooks.PartyAnimal },
-    __Unknown__: { job: "???", name: "???", looks: NpcLooks.MintyJourney },
-} satisfies Record<string, NpcPersona>;
+const npcPersonas = [
+    { internalName: "BalltownOutskirtsMiner", job: "Miner", name: "Dante", looks: NpcLooks.Miner },
+    { internalName: "BalltownOutskirtsFarmer", job: "Farmer", name: "Lars", looks: NpcLooks.Farmer },
+    { internalName: "NewBalltownBallFruitFanatic", job: "Eccentric", name: "Marf", looks: NpcLooks.BallFruitFanatic },
+    { internalName: "NewBalltownArmorer", job: "Armorer", name: "Trav", looks: NpcLooks.Dizzy },
+    { internalName: "NewBalltownFishmonger", job: "Fishmonger", name: "Pop", looks: NpcLooks.Nerd },
+    { internalName: "NewBalltownOliveFanatic", job: "Autist", name: "Oly", looks: NpcLooks.LivingOlive },
+    { internalName: "NewBalltownCroupier", job: "Croupier", name: "Flum", looks: NpcLooks.PartyAnimal },
+    { internalName: "__Unknown__", job: "???", name: "???", looks: NpcLooks.MintyJourney },
+] as const satisfies readonly NpcPersona[];
+
+type InternalPersonaName = typeof npcPersonas[number]["internalName"];
+
+export const NpcPersonas = npcPersonas.reduce((obj, npcPersona) => {
+    obj[npcPersona.internalName] = npcPersona;
+    return obj;
+}, {} as Record<InternalPersonaName, NpcPersona>);
