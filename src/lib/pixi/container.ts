8be01845e8e1fdf1069e81250eb55df2ab9a1e86
Hubol <hubol.gordon@gmail.com>
2025-01-25 11:40:09 -0600

    container can be typed

--- src/lib/pixi/container.ts
+++ src/lib/pixi/container.ts
@@ -1,5 +1,9 @@
 import { Container, DisplayObject } from "pixi.js";
 
+export function container(...children: DisplayObject[]): Container;
+export function container<TDisplayObject extends DisplayObject>(
+    ...children: TDisplayObject[]
+): Container<TDisplayObject>;
 export function container(...children: DisplayObject[]) {
     const c = new Container();
     if (children.length) {
