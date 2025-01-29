8895f60c8ec6c7197b8751eca8afb62e62b3ddd4
hubol <hubol.gordon@gmail.com>
2025-01-29 09:39:04 -0600

    This comment is certainly redundant

--- src/igua/mixins/mxn-speaker.ts
+++ src/igua/mixins/mxn-speaker.ts
@@ -4,7 +4,6 @@ interface MxnSpeakerArgs {
     name: string;
     colorPrimary: number;
     colorSecondary: number;
-    // TODO head position? Or is that another mixin? mxnHasHead??
 }
 
 export function mxnSpeaker(obj: DisplayObject, speaker: MxnSpeakerArgs) {
