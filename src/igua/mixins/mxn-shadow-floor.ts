b6a4fb472f263eb91259262dc56599948d848af0
Hubol <hubol.gordon@gmail.com>
2025-02-08 20:03:05 -0600

    mxnShadowFloor destroys itself when host is destroyed

--- src/igua/mixins/mxn-shadow-floor.ts
+++ src/igua/mixins/mxn-shadow-floor.ts
@@ -15,7 +15,13 @@ const CtxShadowFloorObj = new SceneLocal(() => container().named("CtxShadowFloor
 
 export function mxnShadowFloor(obj: DisplayObject, args: MxnShadowFloorArgs) {
     const shadowObj = Sprite.from(Tx.Light.ShadowIguana).anchored(0.5, 0.5).step(
-        self => self.at(obj.position).add(args.offset),
+        self => {
+            if (obj.destroyed) {
+                self.destroy();
+                return;
+            }
+            self.at(obj.position).add(args.offset);
+        },
         StepOrder.Camera - 1,
     ).mixin(mxnBoilMirrorRotate).show(CtxShadowFloorObj.value);
 
