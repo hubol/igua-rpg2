a1827ffd036f23e16f2dc4918dcc53ae47a62695
Hubol <hubol.gordon@gmail.com>
2025-02-08 20:02:19 -0600

    objIndexedSprite utility ported from xmas24

---
+++ src/igua/objects/utils/obj-indexed-sprite.ts
@@ -0,0 +1,30 @@
+import { Sprite, Texture } from "pixi.js";
+
+export function objIndexedSprite(textures: Texture[], textureIndex = 0) {
+    let effectiveTextureIndex = Math.max(0, Math.min(textures.length - 1, Math.floor(textureIndex)));
+
+    const spr = Sprite.from(textures[effectiveTextureIndex]).merge({
+        get effectiveTextureIndex() {
+            return effectiveTextureIndex;
+        },
+        get textures() {
+            return textures;
+        },
+        set textures(value) {
+            textures = value;
+            this.textureIndex = textureIndex;
+        },
+        get textureIndex() {
+            return textureIndex;
+        },
+        set textureIndex(value) {
+            textureIndex = value;
+            effectiveTextureIndex = Math.max(0, Math.min(textures.length - 1, Math.floor(textureIndex)));
+            spr.texture = textures[effectiveTextureIndex];
+        },
+        textureLength: textures.length,
+    });
+    return spr;
+}
+
+export type ObjIndexedSprite = ReturnType<typeof objIndexedSprite>;
