715ecb10903f94f679a5ffeb7137f7c2cc018968
Hubol <hubol.gordon@gmail.com>
2025-02-25 23:44:50 -0600

    Rework mxnStopAndDieWhenHitGround

--- src/igua/mixins/mxn-stop-and-die-when-hit-ground.ts
+++ src/igua/mixins/mxn-stop-and-die-when-hit-ground.ts
@@ -1,5 +1,4 @@
-import { interp } from "../../lib/game-engine/routines/interp";
-import { sleep, sleepf } from "../../lib/game-engine/routines/sleep";
+import { sleep } from "../../lib/game-engine/routines/sleep";
 import { MxnPhysics } from "./mxn-physics";
 
 export function mxnStopAndDieWhenHitGround(obj: MxnPhysics) {
@@ -13,13 +12,9 @@ export function mxnStopAndDieWhenHitGround(obj: MxnPhysics) {
     })
         .coro(function* (self) {
             yield () => isDying;
-            yield sleep(1000);
-            yield interp(self, "alpha").steps(4).to(0.5).over(500);
+            yield sleep(1500);
+            self.alpha = 0.5;
             yield sleep(500);
-            for (let i = 0; i < 16; i++) {
-                self.visible = !self.visible;
-                yield sleepf(2);
-            }
             self.destroy();
         });
 }
