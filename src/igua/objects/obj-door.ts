e774cdbb162ace2c01b2b8a2f60ae1ef68d7ca93
Hubol <hubol.gordon@gmail.com>
2025-03-04 13:05:56 -0600

    Door locking/unlocking animation

--- src/igua/objects/obj-door.ts
+++ src/igua/objects/obj-door.ts
@@ -1,6 +1,9 @@
-import { Sprite } from "pixi.js";
+import { Graphics, Sprite } from "pixi.js";
 import { Tx } from "../../assets/textures";
 import { EscapeTickerAndExecute } from "../../lib/game-engine/asshat-ticker";
+import { sleepf } from "../../lib/game-engine/routines/sleep";
+import { approachLinear, nlerp } from "../../lib/math/number";
+import { container } from "../../lib/pixi/container";
 import { DramaMisc } from "../drama/drama-misc";
 import { show } from "../drama/show";
 import { Cutscene } from "../globals";
@@ -17,14 +20,41 @@ export function objDoor({ sceneName, checkpointName }: ObjDoorArgs) {
 
     const sceneChanger = SceneChanger.create({ sceneName, checkpointName });
 
-    const obj = Sprite.from(Tx.Door.NormalOpen)
+    const openObj = Sprite.from(Tx.Door.NormalOpen);
+    const maskObj0 = new Graphics().beginFill(0x000000).drawRect(0, 0, 34, 46);
+    const maskObj1 = new Graphics().beginFill(0x000000).drawRect(0, 0, 34, 46);
+    const maskObj2 = new Graphics().beginFill(0x000000).drawRect(0, 0, 34, 46);
+    const lockedObj0 = Sprite.from(Tx.Door.NormalLockedLayer0).masked(maskObj0);
+    const lockedObj1 = Sprite.from(Tx.Door.NormalLockedLayer1).masked(maskObj1);
+    const lockedObj2 = Sprite.from(Tx.Door.NormalLockedLayer2).masked(maskObj2);
+
+    function getTargetMaskY() {
+        return locked ? 0 : 46;
+    }
+
+    function setMaskPositionToTarget() {
+        const targetY = getTargetMaskY();
+        maskObj0.y = targetY;
+        maskObj1.y = targetY;
+        maskObj2.y = targetY;
+    }
+
+    setMaskPositionToTarget();
+
+    const obj = container(openObj, maskObj0, maskObj1, maskObj2, lockedObj0, lockedObj1, lockedObj2)
         .merge({
             get locked() {
                 return locked;
             },
             set locked(value) {
                 locked = value;
-                obj.texture = locked ? Tx.Door.NormalLocked : Tx.Door.NormalOpen;
+                setMaskPositionToTarget();
+            },
+            lock() {
+                locked = true;
+            },
+            unlock() {
+                locked = false;
             },
         })
         .mixin(mxnInteract, () => {
@@ -37,6 +67,33 @@ export function objDoor({ sceneName, checkpointName }: ObjDoorArgs) {
                     DramaMisc.departRoomViaDoor(null);
                     sceneChanger.changeScene();
                 });
+            }
+        })
+        .coro(function* () {
+            while (true) {
+                yield () => maskObj0.y !== getTargetMaskY();
+                while (maskObj0.y !== getTargetMaskY()) {
+                    const targetY = getTargetMaskY();
+                    const rawNextY = approachLinear(maskObj0.y, targetY, 4);
+
+                    // Snap to brick positions
+                    const nextY = rawNextY === targetY ? targetY : (Math.round((rawNextY - 3) / 7) * 7 + 3);
+
+                    const f = nlerp(1, 6, 1 - (Math.abs(nextY - targetY) / 46));
+
+                    const isLocking = maskObj0.y > nextY;
+
+                    const f0 = Math.max(1, 0.3 * f);
+                    const f1 = Math.max(1, 0.65 * f);
+
+                    // TODO locking/unlocking sfx
+                    (isLocking ? maskObj0 : maskObj2).y = nextY;
+                    yield sleepf(f1);
+                    maskObj1.y = nextY;
+                    yield sleepf(f1);
+                    (isLocking ? maskObj2 : maskObj0).y = nextY;
+                    yield sleepf(f0);
+                }
             }
         });
 
