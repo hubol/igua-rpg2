a45af1d616a460272583b4827933eac7a32dcda1
Hubol <hubol.gordon@gmail.com>
2025-03-28 18:56:30 -0500

    More secret shop decoration

--- src/igua/scenes/scn-new-balltown-outskirts-secret-shop.ts
+++ src/igua/scenes/scn-new-balltown-outskirts-secret-shop.ts
@@ -1,5 +1,8 @@
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { Mzk } from "../../assets/music";
+import { Jukebox } from "../core/igua-audio";
 
 export function scnNewBalltownOutskirtsSecretShop() {
+    Jukebox.play(Mzk.OpenWound);
     const lvl = Lvl.NewBalltownOutskirtsSecretShop();
 }
