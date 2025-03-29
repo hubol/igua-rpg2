6fccf23b89b819e1e770505aa37adbb8aef5b9db
Hubol <hubol.gordon@gmail.com>
2025-03-29 02:47:56 -0500

    Thought-provoking artwork

--- src/igua/scenes/scn-new-balltown-underneath-homeowner.ts
+++ src/igua/scenes/scn-new-balltown-underneath-homeowner.ts
@@ -1,6 +1,8 @@
+import { Graphics } from "pixi.js";
 import { Lvl, LvlType } from "../../assets/generated/levels/generated-level-data";
 import { Mzk } from "../../assets/music";
 import { Instances } from "../../lib/game-engine/instances";
+import { interpvr } from "../../lib/game-engine/routines/interp";
 import { sleep } from "../../lib/game-engine/routines/sleep";
 import { Jukebox } from "../core/igua-audio";
 import { DramaMisc } from "../drama/drama-misc";
@@ -14,6 +16,7 @@ export function scnNewBalltownUnderneathHomeowner() {
     Jukebox.play(Mzk.PleasureMafia);
     const lvl = Lvl.NewBalltownUnderneathHomeowner();
     enrichEnemyPresence(lvl);
+    enrichArtwork(lvl);
 }
 
 function enrichEnemyPresence(lvl: LvlType.NewBalltownUnderneathHomeowner) {
@@ -47,9 +50,29 @@ function enrichEnemyPresence(lvl: LvlType.NewBalltownUnderneathHomeowner) {
                 yield scene.camera.auto.panToSubject(self);
                 yield* show("I hope this is a sufficient reward.");
                 yield* DramaQuests.completeQuest("NewBalltownUnderneathHomeownerEnemyPresenceCleared", self);
-                yield* show("Please come see me again. I can heal poison and also create pornographic images.");
+                yield* show("Please come see me again. I can heal poison and also create thought-provoking artworks.");
                 RpgProgress.flags.underneath.homeowner.hasClearedHouseOfEnemies = true;
             }, { speaker: self, camera: { start: "pan-to-speaker" } });
         });
     }
+}
+
+function enrichArtwork(lvl: LvlType.NewBalltownUnderneathHomeowner) {
+    if (RpgProgress.flags.underneath.homeowner.hasClearedHouseOfEnemies) {
+        return;
+    }
+
+    const maskObj = new Graphics()
+        .beginFill(0xffffff)
+        .drawRect(0, 0, scene.level.width, scene.level.height)
+        .at(0, scene.level.height)
+        .coro(function* (self) {
+            yield () => RpgProgress.flags.underneath.homeowner.hasClearedHouseOfEnemies;
+            yield sleep(500);
+            yield interpvr(self).steps(32).to(0, 0).over(2000);
+        })
+        .show();
+
+    lvl.ArtworkFrontGroup.masked(maskObj);
+    lvl.ArtworkTerrainGroup.masked(maskObj);
 }
