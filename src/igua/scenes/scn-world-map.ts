dcb69fa13e1b7beb467c13199c2e0e199f3f040b
Hubol <hubol.gordon@gmail.com>
2025-05-29 11:30:16 -0500

    More map decoration

--- src/igua/scenes/scn-world-map.ts
+++ src/igua/scenes/scn-world-map.ts
@@ -1,12 +1,17 @@
+import { Container, DisplayObject } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
 import { Tx } from "../../assets/textures";
 import { Instances } from "../../lib/game-engine/instances";
+import { mxnBoilPivot } from "../mixins/mxn-boil-pivot";
 import { mxnSinePivot } from "../mixins/mxn-sine-pivot";
 import { OgmoFactory } from "../ogmo/factory";
 
 export function scnWorldMap() {
-    Lvl.WorldMap();
+    const lvl = Lvl.WorldMap();
     Instances(OgmoFactory.createDecal).filter(x => x.texture === Tx.WorldMap.Cloud0).forEach(x =>
         x.mixin(mxnSinePivot)
     );
+    Object.entries(lvl).flatMap(([key, value]) =>
+        key.endsWith("Label") && value instanceof DisplayObject ? [value] : []
+    ).forEach(x => x.mixin(mxnBoilPivot));
 }
