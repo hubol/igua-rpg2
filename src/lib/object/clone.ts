2de40a5639761545fb32cdcc614255eb3b6babd6
Hubol <hubol.gordon@gmail.com>
2025-06-26 18:22:54 -0500

    Note about `structuredClone`

--- src/lib/object/clone.ts
+++ src/lib/object/clone.ts
@@ -1,4 +1,5 @@
 export function clone<T>(obj: T): T {
+    // TODO: note `structuredClone` is only available in Electron 17+
     if ("structuredClone" in window) {
         return structuredClone(obj);
     }
