c706939f24f40f2623629901bf493c23e5d17fff
hubol <hubol.gordon@gmail.com>
2025-07-09 01:10:49 -0500

    `devSimulateLoot`

---
+++ src/igua/dev/dev-simulate-loot.ts
@@ -0,0 +1,69 @@
+import { Integer } from "../../lib/math/number-alias-types";
+import { EquipmentInternalName } from "../data/data-equipment";
+import { DataPocketItemInternalName } from "../data/data-pocket-items";
+import { RpgEnemyRank } from "../rpg/rpg-enemy-rank";
+import { RpgEquipmentEffects } from "../rpg/rpg-equipment-effects";
+import { RpgLoot } from "../rpg/rpg-loot";
+
+export function devSimulateLoot(loot: RpgLoot.Model) {
+    const { status } = RpgEnemyRank.create({});
+    const effects = RpgEquipmentEffects.create();
+
+    const counts = {
+        equipments: new Map<EquipmentInternalName, Integer>(),
+        flops: new Map<Integer, Integer>(),
+        pocketItems: new Map<DataPocketItemInternalName, Integer>(),
+        valuables: new Map<Integer, Integer>(),
+    };
+
+    const iterationsCount = 100000;
+
+    for (let i = 0; i < iterationsCount; i++) {
+        const drop = RpgLoot.Methods.drop(loot, status, effects.loot);
+        increment(counts.equipments, drop.equipments);
+        increment(counts.flops, drop.flops);
+        increment(counts.pocketItems, drop.pocketItems);
+        increment(counts.valuables, drop.valuables);
+    }
+
+    console.log(`====Loot simulation result (${iterationsCount} iterations)====
+${print({ iterationsCount, map: counts.equipments, name: "Equipment", serializer: (item) => item })}
+${print({ iterationsCount, map: counts.flops, name: "Flops", serializer: (item) => `#${item + 1}` })}
+${print({ iterationsCount, map: counts.pocketItems, name: "Pocket Items", serializer: (item) => item })}
+${print({ iterationsCount, map: counts.valuables, name: "Valuables", serializer: (item) => String(item) })}`);
+}
+
+interface PrintArgs<T> {
+    iterationsCount: Integer;
+    map: Map<T, Integer>;
+    name: string;
+    serializer: (item: T) => string;
+}
+
+function print<T>(args: PrintArgs<T>) {
+    const orderedItems = [...args.map.entries()]
+        .map(([item, count]) => ({ item, count }))
+        .sort((a, b) => a.count - b.count);
+    return `----${args.name}----
+${
+        orderedItems.map(({ item, count }) =>
+            `${args.serializer(item)}: ${count} (${((count / args.iterationsCount) * 100).toFixed(3)}%)`
+        ).join("\n")
+    }`;
+}
+
+function increment(map: Map<unknown, Integer>, value: unknown) {
+    if (Array.isArray(value)) {
+        for (const item of value) {
+            incrementImpl(map, item);
+        }
+        return;
+    }
+
+    incrementImpl(map, value);
+}
+
+function incrementImpl(map: Map<unknown, Integer>, value: unknown) {
+    const count = (map.get(value) ?? 0) + 1;
+    map.set(value, count);
+}
