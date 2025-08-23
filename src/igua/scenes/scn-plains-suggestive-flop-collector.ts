17d7399666425ce3f974d260cb554c566ed2c498
Hubol <hubol.gordon@gmail.com>
2025-08-22 21:05:15 -0500

    Very WIP flop collector

---
+++ src/igua/scenes/scn-plains-suggestive-flop-collector.ts
@@ -0,0 +1,48 @@
+import { Lvl, LvlType } from "../../assets/generated/levels/generated-level-data";
+import { sleep } from "../../lib/game-engine/routines/sleep";
+import { Rng } from "../../lib/math/rng";
+import { vlerp } from "../../lib/math/vector";
+import { vnew } from "../../lib/math/vector-type";
+import { DramaInventory } from "../drama/drama-inventory";
+import { DevKey, scene } from "../globals";
+import { mxnCutscene } from "../mixins/mxn-cutscene";
+import { objFlop } from "../objects/obj-flop";
+import { Rpg } from "../rpg/rpg";
+
+export function scnPlainsSuggestiveFlopCollector() {
+    const lvl = Lvl.PlainsSuggestiveFlopCollector();
+    enrichCollectorNpc(lvl);
+
+    scene.stage.step(() => {
+        if (DevKey.isDown("KeyF")) {
+            Rpg.inventory.keyItems.receive("FlopBlindBox");
+        }
+    });
+}
+
+function enrichCollectorNpc(lvl: LvlType.PlainsSuggestiveFlopCollector) {
+    const minimumToReachEnd = 6;
+
+    lvl.CollectorNpc.mixin(mxnCutscene, function* () {
+        const flopsCount = yield* DramaInventory.askRemoveCount("How many flops can we unbox together?", {
+            kind: "key_item",
+            id: "FlopBlindBox",
+        });
+
+        if (!flopsCount) {
+            return;
+        }
+
+        const v1 = vnew();
+
+        for (let i = 0; i < flopsCount; i++) {
+            const position = vlerp(
+                v1.at(lvl.FlopStartMarker),
+                lvl.FlopEndMarker,
+                i / Math.max(minimumToReachEnd, flopsCount),
+            );
+            objFlop(Rng.intc(0, 998)).at(position).show();
+            yield sleep(100);
+        }
+    });
+}
