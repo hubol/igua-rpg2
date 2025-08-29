8ef5fb8d6550cdc15f3e40f2014f070c75f42357
Hubol <hubol.gordon@gmail.com>
2025-08-29 17:23:27 -0500

    Re-order physical and poison damage

--- src/igua/objects/overlay/obj-health-bar.ts
+++ src/igua/objects/overlay/obj-health-bar.ts
@@ -3,9 +3,9 @@ import { objStatusBar } from "./obj-status-bar";
 
 const DamageIndex = {
     // TODO should non-status damage types show their own damage chunks? digits?
-    [RpgStatus.DamageKind.Emotional]: 0,
-    [RpgStatus.DamageKind.Physical]: 0,
-    [RpgStatus.DamageKind.Poison]: 1,
+    [RpgStatus.DamageKind.Emotional]: 1,
+    [RpgStatus.DamageKind.Physical]: 1,
+    [RpgStatus.DamageKind.Poison]: 0,
 };
 
 export function objHealthBar(width: number, height: number, value: number, maxValue: number) {
@@ -18,20 +18,20 @@ export function objHealthBar(width: number, height: number, value: number, maxVa
         value,
         decreases: [
             {
-                tintBar: 0x800000,
-                digit: {
-                    signed: false,
-                    size: "medium",
-                    tint: 0xffffff,
-                    align: "right",
-                },
-            },
-            {
                 tintBar: 0x008000,
                 digit: {
                     signed: false,
                     size: "medium",
                     tint: 0x004000,
+                    align: "right",
+                },
+            },
+            {
+                tintBar: 0x800000,
+                digit: {
+                    signed: false,
+                    size: "medium",
+                    tint: 0xffffff,
                     align: "right",
                 },
             },
@@ -51,7 +51,7 @@ export function objHealthBar(width: number, height: number, value: number, maxVa
         .merge({
             effects: {
                 healed(value, delta) {
-                    bar.increase(value, delta, 0);
+                    bar.increase(value, delta, 1);
                 },
                 tookDamage(value, delta, kind) {
                     bar.decrease(value, Math.abs(delta), DamageIndex[kind]);
