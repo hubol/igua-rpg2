6be5bead2ddf6b252988f2f63f6e009aa25b83ac
Hubol <hubol.gordon@gmail.com>
2025-08-30 17:06:57 -0500

    Continued `objItemRescueAngel`

--- src/igua/objects/characters/obj-item-rescue-angel.ts
+++ src/igua/objects/characters/obj-item-rescue-angel.ts
@@ -1,11 +1,16 @@
-import { Graphics, Sprite } from "pixi.js";
+import { DisplayObject, Graphics, Sprite } from "pixi.js";
 import { Tx } from "../../../assets/textures";
 import { factor, interp, interpvr } from "../../../lib/game-engine/routines/interp";
 import { sleep } from "../../../lib/game-engine/routines/sleep";
+import { approachLinear } from "../../../lib/math/number";
+import { vdir } from "../../../lib/math/vector";
+import { VectorSimple, vnew } from "../../../lib/math/vector-type";
 import { container } from "../../../lib/pixi/container";
 import { scene } from "../../globals";
 import { mxnBoilPivot } from "../../mixins/mxn-boil-pivot";
+import { mxnPhysics } from "../../mixins/mxn-physics";
 import { mxnSinePivot } from "../../mixins/mxn-sine-pivot";
+import { StepOrder } from "../step-order";
 import { objIndexedSprite } from "../utils/obj-indexed-sprite";
 
 const [propellerTx0, propellerTx1, propellerTx2, bagTx, legsTx, armTx, dressTx, nogginTx, faceTx] = Tx.Characters
@@ -14,6 +19,10 @@ const [propellerTx0, propellerTx1, propellerTx2, bagTx, legsTx, armTx, dressTx, 
     });
 
 function objItemRescueAngelPuppet() {
+    const controls = {
+        animatePivot: true,
+    };
+
     const coros = {
         *removeBag() {
             yield interp(bagObj, "alpha").steps(2).to(0).over(333);
@@ -37,14 +46,93 @@ function objItemRescueAngelPuppet() {
         Sprite.from(nogginTx),
         Sprite.from(faceTx).mixin(mxnSinePivot),
     )
-        .step(self => self.pivot.y = Math.round(Math.sin(scene.ticker.ticks * 0.1) * 3))
-        .merge({ coros });
+        .step(self => {
+            if (controls.animatePivot) {
+                self.pivot.y = Math.round(Math.sin(scene.ticker.ticks * 0.1) * 3);
+            }
+        })
+        .merge({ controls, coros });
 }
 
-export function objItemRescueAngel() {
-    return objItemRescueAngelPuppet()
+const v = vnew();
+
+export function objItemRescueAngel(rescueObj: DisplayObject) {
+    function getTargetPosition() {
+        return v.at(rescueObj.getWorldPosition());
+    }
+
+    const puppetObj = objItemRescueAngelPuppet();
+
+    return container(puppetObj)
+        .pivoted(21, 65)
+        .mixin(mxnPhysics, { gravity: 0, physicsRadius: 8 })
         .coro(function* (self) {
-            yield sleep(5000);
-            yield* self.coros.removeBag();
+            self.physicsEnabled = false;
+
+            const aliveBehaviorObj = container()
+                .merge({ direction: Math.PI, targetDirection: Math.PI, speed: 1, previousDistance: Number.MAX_VALUE })
+                .step((aliveBehaviorObj) => {
+                    if (rescueObj.destroyed) {
+                        aliveBehaviorObj.destroy();
+                        return;
+                    }
+
+                    if ("speed" in rescueObj && "x" in (rescueObj.speed as VectorSimple)) {
+                        (rescueObj.speed as VectorSimple).x = 0;
+                        (rescueObj.speed as VectorSimple).y = 0;
+                    }
+
+                    if (rescueObj.is(mxnPhysics)) {
+                        rescueObj.physicsEnabled = false;
+                        rescueObj.gravity = 0;
+                    }
+
+                    if (aliveBehaviorObj.speed > 0) {
+                        const offset = getTargetPosition().add(self, -1);
+
+                        aliveBehaviorObj.targetDirection = vdir(offset);
+
+                        const distance = offset.vlength;
+                        const noise = distance < 60
+                            ? 0
+                            : Math.sin(scene.ticker.ticks * 0.1) * Math.min(distance / 120, Math.PI / 4);
+
+                        aliveBehaviorObj.direction = approachLinear(
+                            aliveBehaviorObj.direction,
+                            aliveBehaviorObj.targetDirection + noise,
+                            Math.PI / 16,
+                        );
+
+                        self.speed
+                            .at(Math.cos(aliveBehaviorObj.direction), -Math.sin(aliveBehaviorObj.direction))
+                            .scale(aliveBehaviorObj.speed);
+
+                        aliveBehaviorObj.previousDistance = distance;
+                    }
+                })
+                .coro(function* (aliveBehaviorObj) {
+                    yield () => aliveBehaviorObj.previousDistance < 60;
+                    aliveBehaviorObj.speed = 0;
+                    self.speed.at(0, 0);
+                    yield interpvr(self).factor(factor.sine).to(rescueObj).over(500);
+                    puppetObj.controls.animatePivot = false;
+                    aliveBehaviorObj.step(() => {
+                        if (!rescueObj.destroyed) {
+                            rescueObj.at(self);
+                        }
+                    }, StepOrder.AfterPhysics);
+                    self.speed.at(0, -0.2);
+                })
+                .show(self);
+
+            yield () => aliveBehaviorObj.destroyed;
+            aliveBehaviorObj.destroy();
+            yield* puppetObj.coros.removeBag();
+
+            self.speed.at(0, -0.5);
+            self.step(self => self.speed.y -= 0.1);
+
+            yield () => self.y <= -100;
+            self.destroy();
         });
 }
