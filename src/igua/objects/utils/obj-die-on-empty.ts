6c449c24ffb6025a3e24ac72c3b3083f47d3e64a
Hubol <hubol.gordon@gmail.com>
2025-09-02 21:47:08 -0500

    Increased neuroses in `objDieOnEmpty`

--- src/igua/objects/utils/obj-die-on-empty.ts
+++ src/igua/objects/utils/obj-die-on-empty.ts
@@ -1,11 +1,12 @@
+import { holdf } from "../../../lib/game-engine/routines/hold";
 import { container } from "../../../lib/pixi/container";
 import { StepOrder } from "../step-order";
 
 export function objDieOnEmpty() {
     return container()
-        .step(self => {
-            if (self.children.length === 0) {
-                self.destroy();
-            }
+        .coro(function* (self) {
+            // Particularly neurotic implementation
+            yield holdf(() => self.children.length === 0, 3);
+            self.destroy();
         }, StepOrder.BeforeCamera);
 }
