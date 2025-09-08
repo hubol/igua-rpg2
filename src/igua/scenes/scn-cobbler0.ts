9f8e42e9f389b61f9167aa182dcffee2ade69c8b
Hubol <hubol.gordon@gmail.com>
2025-09-07 23:17:02 -0500

    More cobbler work

--- src/igua/scenes/scn-cobbler0.ts
+++ src/igua/scenes/scn-cobbler0.ts
@@ -1,5 +1,6 @@
 import { Lvl, LvlType } from "../../assets/generated/levels/generated-level-data";
 import { dramaShop } from "../drama/drama-shop";
+import { ask, show } from "../drama/show";
 import { mxnCutscene } from "../mixins/mxn-cutscene";
 import { RpgAttack } from "../rpg/rpg-attack";
 
@@ -25,6 +26,25 @@ function enrichGlueDripSources(lvl: LvlType.Cobbler0) {
 
 function enrichGluemaker(lvl: LvlType.Cobbler0) {
     lvl.GlueMakerNpc.mixin(mxnCutscene, function* () {
-        yield* dramaShop("Gluemaker", { primaryTint: 0x100327, secondaryTint: 0xD1EBFF });
+        const result = yield* ask(
+            "I make the glue around here! What can I do for ya?",
+            "Buy glue",
+            "About glue",
+            "Nothing, sorry!",
+        );
+
+        if (result === 0) {
+            yield* dramaShop("Gluemaker", { primaryTint: 0x100327, secondaryTint: 0xD1EBFF });
+        }
+        else if (result === 1) {
+            yield* show(
+                "Oh, you don't know what glue is good for?",
+                "You can use shoe glue to combine two of the same shoes into one, powering them up.",
+                "And I will trade it for several currencies. Please take a look.",
+            );
+        }
+        else if (result === 2) {
+            yield* show("No need to apologize, sucka!");
+        }
     });
 }
