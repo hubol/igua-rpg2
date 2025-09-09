685aea92af9d0e9faaa3bcf7ad09b1bac50ea5ba
Hubol <hubol.gordon@gmail.com>
2025-09-09 14:10:32 -0500

    Attempt to treat only number arrays as vectors

--- src/lib/extensions/js-array-vector.ts
+++ src/lib/extensions/js-array-vector.ts
@@ -1,7 +1,20 @@
 import { defineVectorProperties, Vector } from "../math/vector-type";
 
 declare global {
-    interface Array<T> extends Vector {}
+    interface Array<T> {
+        x: T extends number ? Vector["x"] : never;
+        y: T extends number ? Vector["y"] : never;
+        vcpy: T extends number ? Vector["vcpy"] : never;
+        vround: T extends number ? Vector["vround"] : never;
+        vclamp: T extends number ? Vector["vclamp"] : never;
+        add: T extends number ? Vector["add"] : never;
+        moveTowards: T extends number ? Vector["moveTowards"] : never;
+        normalize: T extends number ? Vector["normalize"] : never;
+        scale: T extends number ? Vector["scale"] : never;
+        at: T extends number ? Vector["at"] : never;
+        vlength: T extends number ? Vector["vlength"] : never;
+        isZero: T extends number ? Vector["isZero"] : never;
+    }
 }
 
 defineVectorProperties(Array.prototype, { omit: ["at"] });
@@ -39,3 +52,7 @@ Object.defineProperties(Array.prototype, {
         configurable: true,
     },
 });
+
+const assertAssignableToVector: Vector = [0, 0];
+// @ts-expect-error
+const assertCantUseVectorMethodsOnNonNumberArray = ["hi"].scale(1, 1);
