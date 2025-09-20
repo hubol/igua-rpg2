e5951e63dbcb6278a729bf657df8baacde78ef03
Hubol <hubol.gordon@gmail.com>
2025-09-19 19:02:58 -0500

    Fix bugs when updating wetness tints

--- src/igua/rpg/rpg-status.ts
+++ src/igua/rpg/rpg-status.ts
@@ -221,7 +221,7 @@ export namespace RpgStatus {
                         target.conditions.wetness.tint = blendColorDelta(
                             target.conditions.wetness.tint,
                             attack.conditions.wetness.tint,
-                            Math.min(255, Math.max(attack.conditions.wetness.tint, 4)),
+                            Math.min(255, 4 + Math.max(attack.conditions.wetness.value - 30, 0)),
                         );
                     }
                 }
