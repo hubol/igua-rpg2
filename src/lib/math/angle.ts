3181ed93315c9fc341bd70767dce774e40ca113f
Hubol <hubol.gordon@gmail.com>
2025-10-07 19:53:22 -0500

    `vrad`

--- src/lib/math/angle.ts
+++ src/lib/math/angle.ts
@@ -10,3 +10,7 @@ export const ToRad = Math.PI / 180;
 export function vdeg(degrees: DegreesFloat) {
     return vnew().at(Math.cos(degrees * ToRad), Math.sin(degrees * ToRad));
 }
+
+export function vrad(radians: number) {
+    return vnew().at(Math.cos(radians), Math.sin(radians));
+}
