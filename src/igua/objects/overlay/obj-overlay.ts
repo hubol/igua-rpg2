4840d9ab0d11311fcba1f6114b33c3d415ab9ce3
Hubol <hubol.gordon@gmail.com>
2025-11-25 12:26:55 -0600

    overlay exposes a `solidBelowMessages`

--- src/igua/objects/overlay/obj-overlay.ts
+++ src/igua/objects/overlay/obj-overlay.ts
@@ -11,16 +11,30 @@ export function objOverlay() {
     const enemyHealthBarsObj = objEnemyHealthBars();
     const messages = container().named("Messages").step(self => self.y = hudObj.effectiveHeight + 3, 1);
     const dev = container().named("Dev");
-    const solidObj = objSolidOverlay();
+    const solidObjBelowMessages = objSolidOverlay();
+    const solidObjAboveMessages = objSolidOverlay();
     const systemMessageObj = objSystemMessage();
     const inventoryObj = objUiInventory();
 
-    return container(hudObj, enemyHealthBarsObj, messages, solidObj, systemMessageObj, dev, inventoryObj)
+    return container(
+        hudObj,
+        enemyHealthBarsObj,
+        solidObjBelowMessages,
+        messages,
+        solidObjAboveMessages,
+        systemMessageObj,
+        dev,
+        inventoryObj,
+    )
         .merge({
             hud: hudObj as Omit<ObjHud, keyof Container>,
             messages,
             dev,
-            solid: solidObj as Pick<ObjSolidOverlay, "fadeIn" | "fadeOut" | "tint" | "blendMode">,
+            solidBelowMessages: solidObjBelowMessages as Pick<
+                ObjSolidOverlay,
+                "fadeIn" | "fadeOut" | "tint" | "blendMode"
+            >,
+            solid: solidObjAboveMessages as Pick<ObjSolidOverlay, "fadeIn" | "fadeOut" | "tint" | "blendMode">,
             enemyHealthBars: enemyHealthBarsObj as Pick<ObjEnemyHealthBars, "getRpgStatusEffects">,
             systemMessage: systemMessageObj as Pick<ObjSystemMessage, "setMessage">,
             inventory: inventoryObj as ObjUiInventory,
