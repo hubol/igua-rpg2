e1cc34e7f119608a945021d63ed421c63173bcb7
Hubol <hubol.gordon@gmail.com>
2025-11-25 00:24:57 -0600

    Bizarre helium gimmick

--- src/igua/scenes/scn-efficient-home.ts
+++ src/igua/scenes/scn-efficient-home.ts
@@ -1,9 +1,22 @@
-import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { Lvl, LvlType } from "../../assets/generated/levels/generated-level-data";
 import { scene } from "../globals";
+import { objHeliumExhaust } from "../objects/nature/obj-helium-exhaust";
 import { playerObj } from "../objects/obj-player";
+import { Rpg } from "../rpg/rpg";
 
 export function scnEfficientHome() {
-    Lvl.EfficientHome();
+    const lvl = Lvl.EfficientHome();
     scene.camera.at(Math.floor(playerObj.x / 512) * 512, Math.floor(playerObj.y / 288) * 288);
     scene.camera.mode = "controlled";
+
+    enrichHelium(lvl);
+}
+
+function enrichHelium(lvl: LvlType.EfficientHome) {
+    [lvl.HeliumMarker0, lvl.HeliumMarker1, lvl.HeliumMarker2].forEach((obj, index) =>
+        objHeliumExhaust()
+            .step(self => self.isAttackActive = Rpg.character.status.conditions.helium.ballons.length === index, -1)
+            .at(obj)
+            .show()
+    );
 }
