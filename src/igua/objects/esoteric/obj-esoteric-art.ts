a4ef58a055224303c1dbf610f04824a377401ed3
hubol <hubol.gordon@gmail.com>
2025-11-25 22:37:25 -0600

    Beautiful art

--- src/igua/objects/esoteric/obj-esoteric-art.ts
+++ src/igua/objects/esoteric/obj-esoteric-art.ts
@@ -1,4 +1,4 @@
-import { Graphics } from "pixi.js";
+import { Graphics, Sprite } from "pixi.js";
 import { Tx } from "../../../assets/textures";
 import { blendColor } from "../../../lib/color/blend-color";
 import { Integer } from "../../../lib/math/number-alias-types";
@@ -30,6 +30,16 @@ export function objEsotericArt(seed: Integer) {
     const palette = range(rng.intc(3, 6)).map(() => rng.color());
     const blendedCount = rng.intc(0, 2);
 
+    function generateSprite() {
+        return Sprite.from(rng.item(shapes))
+            .anchored(0.5, 0.5)
+            .tinted(rng.item(palette))
+            .flipH(rng.choose(1, -1))
+            .flipV(rng.choose(1, -1))
+            .angled(rng.intc(0, 3) * 90)
+            .at(rng.intc(consts.width), rng.intc(consts.height));
+    }
+
     for (let i = 0; i < blendedCount; i++) {
         const [firstColor, secondColor] = rng.shuffle(palette);
         const blendedColor = blendColor(firstColor ?? 0, secondColor ?? 0, rng.float(0.4, 0.6));
@@ -40,6 +50,7 @@ export function objEsotericArt(seed: Integer) {
 
     const obj = container(
         new Graphics().beginFill(rng.item(palette)).drawRect(0, 0, consts.width, consts.height),
+        ...range(rng.intc(24, 48)).map(generateSprite),
     );
 
     return container(obj, mask).masked(mask);
