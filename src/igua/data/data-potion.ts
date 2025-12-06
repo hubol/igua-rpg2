d4ba4b4e217f3d07f2508ea49f157defbe1cb060
Hubol <hubol.gordon@gmail.com>
2025-12-06 14:30:40 -0600

    Many ridiculous potion sound effects

--- src/igua/data/data-potion.ts
+++ src/igua/data/data-potion.ts
@@ -1,5 +1,7 @@
 import { Texture } from "pixi.js";
+import { Sfx } from "../../assets/sounds";
 import { Tx } from "../../assets/textures";
+import { Sound } from "../../lib/game-engine/audio/sound";
 import { RgbInt } from "../../lib/math/number-alias-types";
 import { playerObj } from "../objects/obj-player";
 import { Rpg } from "../rpg/rpg";
@@ -12,6 +14,7 @@ export namespace DataPotion {
         description: string;
         stinkLineTint: RgbInt;
         texture: Texture | null;
+        sound: Sound | null;
     }
 
     export const Manifest = DataLib.createManifest(
@@ -21,78 +24,91 @@ export namespace DataPotion {
                 description: "Delicious nectar. Increases maximum HP.",
                 stinkLineTint: 0x004AFF,
                 texture: Tx.Collectibles.Potion.AttributeHealthUp,
+                sound: Sfx.Effect.Potion.AttributeHealthUp,
             },
             AttributeIntelligenceUp: {
                 name: "Foul Stew",
                 description: "Challenging, odiforous soup. Increases intelligence.",
                 stinkLineTint: 0xCEA5AA,
                 texture: Tx.Collectibles.Potion.AttributeIntelligenceUp,
+                sound: Sfx.Effect.Potion.AttributeIntelligenceUp,
             },
             AttributeStrengthUp: {
                 name: "Claw Powder",
                 description: "Fine grit for sharpening claws. Increases physical attack power.",
                 stinkLineTint: 0xB6B2FF,
                 texture: Tx.Collectibles.Potion.AttributeStrengthUp,
+                sound: Sfx.Effect.Potion.AttributeStrengthUp,
             },
             RestoreHealth: {
                 name: "Sweet Berry",
                 description: "Pungent fruit. Recover some health.",
                 stinkLineTint: 0xD882D1,
                 texture: Tx.Collectibles.Potion.RestoreHealth,
+                sound: Sfx.Effect.Potion.RestoreHealth,
             },
             RestoreHealthRestaurantLevel0: {
                 name: "Pathetic Meal",
                 description: "Meal for a cheapskate. Recover negligible health.",
                 stinkLineTint: 0xD882D1,
                 texture: Tx.Collectibles.Potion.PatheticMeal,
+                sound: Sfx.Effect.Potion.RestoreHealthBagged0,
             },
             RestoreHealthRestaurantLevel1: {
                 name: "Unremarkable Meal",
                 description: "Average meal. Recover some health.",
                 stinkLineTint: 0xD882D1,
                 texture: Tx.Collectibles.Potion.UnremarkableMeal,
+                sound: Sfx.Effect.Potion.RestoreHealthBagged1,
             },
             RestoreHealthRestaurantLevel2: {
                 name: "Celebratory Meal",
                 description: "Oversized meal. Recover substantial health.",
                 stinkLineTint: 0xD882D1,
                 texture: Tx.Collectibles.Potion.CelebratoryMeal,
+                sound: Sfx.Effect.Potion.RestoreHealthBagged2,
             },
             Poison: {
                 name: "Poison",
                 description: "Popular in most communities. Become poisoned.",
                 stinkLineTint: 0xA53609,
                 texture: Tx.Collectibles.Potion.Poison,
+                sound: Sfx.Effect.Potion.Poison,
             },
             PoisonRestore: {
                 name: "Bitter Medicine",
                 description: "Expensive capsule. Recover from poison.",
                 stinkLineTint: 0x7A42FF,
                 texture: Tx.Collectibles.Potion.PoisonRestore,
+                sound: Sfx.Effect.Potion.PoisonRestore,
             },
             Ballon: {
                 name: "Ballon Fruit",
                 description: "Naturally-occurring and filled with helium. What?",
                 stinkLineTint: 0xE1282C,
                 texture: Tx.Collectibles.Potion.Ballon,
+                sound: null,
             },
             Wetness: {
                 name: "TheWetter",
                 description: "Celebrated beverage. Become drenched.",
                 stinkLineTint: 0x2149FF,
                 texture: Tx.Collectibles.Potion.Wetness,
+                sound: Sfx.Effect.Potion.Wetness,
             },
             ForgetLooseValuableCollection: {
                 name: "Forgeddit Fruid Snags",
                 description: "Renowned flavor and texture. Loose valuables forgive your transgressions.",
                 stinkLineTint: 0x808080,
                 texture: Tx.Collectibles.Potion.FruitSnacks,
+                sound: Sfx.Effect.Potion.LooseValuablesForget,
             },
             __Fallback__: {
                 name: "???",
                 description: "Consume to experience a bug",
                 stinkLineTint: 0xff00ff,
                 texture: null,
+                sound: null,
             },
         } satisfies Record<string, Model>,
     );
@@ -103,6 +119,8 @@ export namespace DataPotion {
 
     // TODO The colocation is good. But there are many strange things going on here, haha
     export function usePotion(id: Id) {
+        getById(id).sound?.play();
+
         switch (id) {
             case "AttributeHealthUp":
                 const previousHealthMax = Rpg.character.status.healthMax;
