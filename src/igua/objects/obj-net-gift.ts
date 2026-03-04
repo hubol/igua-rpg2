a1b3a91457311159934d75d134c61b560cf0742a
Hubol <hubol.gordon@gmail.com>
2026-03-03 20:18:43 -0600

    Can actually offer equipment and have it removed from inventory

--- src/igua/objects/obj-net-gift.ts
+++ src/igua/objects/obj-net-gift.ts
@@ -1,11 +1,14 @@
 import { Graphics } from "pixi.js";
 import { onMutate } from "../../lib/game-engine/routines/on-mutate";
+import { Rng } from "../../lib/math/rng";
 import { container } from "../../lib/pixi/container";
 import { DataItem } from "../data/data-item";
 import { DramaInventory } from "../drama/drama-inventory";
+import { DramaItem } from "../drama/drama-item";
 import { show } from "../drama/show";
 import { mxnCutscene } from "../mixins/mxn-cutscene";
 import { IguaClient } from "../net/igua-client";
+import { Rpg } from "../rpg/rpg";
 import { RpgInventory } from "../rpg/rpg-inventory";
 
 export function objNetGift(client: IguaClient) {
@@ -30,10 +33,44 @@ export function objNetGift(client: IguaClient) {
             const giftItem = client.room.giftItem;
 
             if (!giftItem) {
-                yield* show("Gifting item...");
-                const transaction = client.offer({ id: "AnnoyIguanas", kind: "potion" });
-                yield () => Boolean(transaction.outcome);
-                yield* show(transaction.outcome!.accepted ? "Success" : "Someone beat you to it.");
+                const options = Rng.shuffle([...Rpg.inventory.equipment.list])
+                    .filter(equipment => equipment.loadoutIndex === null)
+                    .map(({ equipmentId, level }) => ({
+                        kind: "equipment" as const,
+                        id: equipmentId,
+                        level,
+                    }))
+                    .slice(0, 7)
+                    .map((item) => ({ item, message: DataItem.getName(item) }));
+
+                const item = yield* DramaItem.choose({
+                    message: "Which shoe to offer?",
+                    noneMessage: "None!",
+                    options,
+                });
+
+                if (item === null) {
+                    return;
+                }
+
+                // TODO shitty, should be a better API!
+                const obtainedId = Rpg.inventory.equipment.list.find(equipment =>
+                    equipment.equipmentId === item.id && equipment.level === item.level
+                )?.id ?? -1;
+
+                Rpg.inventory.equipment.remove(obtainedId);
+
+                const transaction = client.offer(item);
+                yield () =>
+                    Boolean(transaction.outcome);
+
+                const outcome = transaction.outcome!;
+
+                yield* show(outcome.accepted ? "Success" : "Someone beat you to it.");
+
+                if (!outcome.accepted) {
+                    yield* DramaInventory.receiveItems([item]);
+                }
             }
             else {
                 const transaction = client.take();
