46573488dfeaf7cdd3f716263aa0a600c0083841
Hubol <hubol.gordon@gmail.com>
2026-03-09 21:54:47 -0500

    Open and close load and new doors

--- src/igua/rpg/rpg-save-files.ts
+++ src/igua/rpg/rpg-save-files.ts
@@ -1,21 +1,21 @@
 import { Integer } from "../../lib/math/number-alias-types";
+import { NpcLooks } from "../data/data-npc-looks";
 import { IguanaLooks } from "../iguana/looks";
 
 export namespace RpgSaveFiles {
     export function check(): check.Model {
         return {
-            lastLoaded: null,
-            saveFilesCount: 0,
+            lastLoadedIndex: null,
+            saveFiles: [
+                { looks: NpcLooks.AustraliasOwnBluey },
+            ],
         };
     }
 
     export namespace check {
         export interface Model {
-            lastLoaded: {
-                index: Integer;
-                looks: IguanaLooks.Serializable;
-            } | null;
-            saveFilesCount: Integer;
+            lastLoadedIndex: Integer | null;
+            saveFiles: ReadonlyArray<{ looks: IguanaLooks.Serializable } | null>;
         }
     }
 }
--- src/igua/scenes/scn-menu-title-screen.ts
+++ src/igua/scenes/scn-menu-title-screen.ts
@@ -1,9 +1,11 @@
 import { Container } from "pixi.js";
 import { objText } from "../../assets/fonts";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { Logger } from "../../lib/game-engine/logger";
 import { vnew } from "../../lib/math/vector-type";
 import { ZIndex } from "../core/scene/z-index";
 import { scene } from "../globals";
+import { objIguanaPuppet } from "../iguana/obj-iguana-puppet";
 import { mxnBoilPivot } from "../mixins/mxn-boil-pivot";
 import { mxnHasHead } from "../mixins/mxn-has-head";
 import { mxnHudModifiers } from "../mixins/mxn-hud-modifiers";
@@ -15,13 +17,10 @@ import { getInitialRpgProgress } from "../rpg/rpg-progress";
 import { RpgSaveFiles } from "../rpg/rpg-save-files";
 
 export function scnMenuTitleScreen() {
-    const checkedSaveFiles = RpgSaveFiles.check();
+    const { saveFiles, lastLoadedSaveFile, errors } = validateLooks(RpgSaveFiles.check());
 
     if (!["new", "load", "fromNew", "fromLoad"].includes(Rpg.character.position.checkpointName)) {
         setRpgProgressData(getInitialRpgProgress());
-    }
-    if (checkedSaveFiles.lastLoaded) {
-        Rpg.character.looks = checkedSaveFiles.lastLoaded.looks;
     }
 
     const lvl = Lvl.MenuTitleScreen();
@@ -30,6 +29,16 @@ export function scnMenuTitleScreen() {
         .step(() => {
             scene.camera.mode = "controlled";
             scene.camera.x = Math.floor(playerObj.x / 500) * 500;
+        }, StepOrder.BeforeCamera)
+        .coro(function* () {
+            objText.MediumBold(
+                errors.map(error => `(!) ${error}`).join("\n"),
+                { tint: 0xff4c4c },
+            )
+                .at(scene.camera)
+                .add(2, 2)
+                .zIndexed(ZIndex.Entities)
+                .show();
         }, StepOrder.BeforeCamera);
 
     scene.stage
@@ -44,20 +53,52 @@ export function scnMenuTitleScreen() {
 
     lvl.ContinueDoor
         .mixin(mxnLabeled, "Continue")
-        .objDoor.locked = checkedSaveFiles.lastLoaded === null;
+        .objDoor.locked = lastLoadedSaveFile === null;
 
     lvl.NewDoor
         .mixin(mxnLabeled, "New");
 
     lvl.LoadDoor
         .mixin(mxnLabeled, "Load")
-        .objDoor.locked = checkedSaveFiles.saveFilesCount < 1;
+        .objDoor.locked = !saveFiles.some(Boolean);
 
     [lvl.NewFile0Door, lvl.NewFile1Door, lvl.NewFile2Door]
-        .forEach((obj, i) => obj.mixin(mxnLabeled, "File " + (i + 1)));
+        .forEach((obj, i) => obj.mixin(mxnLabeled, "File " + (i + 1)).objDoor.locked = Boolean(saveFiles[i]));
 
     [lvl.LoadFile0Door, lvl.LoadFile1Door, lvl.LoadFile2Door]
-        .forEach((obj, i) => obj.mixin(mxnLabeled, "File " + (i + 1)));
+        .forEach((obj, i) => obj.mixin(mxnLabeled, "File " + (i + 1)).objDoor.locked = !Boolean(saveFiles[i]));
+}
+
+interface ValidateLooksResult {
+    lastLoadedSaveFile: RpgSaveFiles.check.Model["saveFiles"][number];
+    saveFiles: RpgSaveFiles.check.Model["saveFiles"];
+    errors: string[];
+}
+
+function validateLooks(check: RpgSaveFiles.check.Model): ValidateLooksResult {
+    const errors = new Array<string>();
+    const validatedSaveFiles = check.saveFiles.map((file, i) => {
+        if (!file) {
+            return file;
+        }
+
+        try {
+            objIguanaPuppet(file.looks);
+            return file;
+        }
+        catch (e) {
+            Logger.logUnexpectedError("validateLooks", e as Error);
+            errors.push(`File ${i + 1} is corrupted.`);
+        }
+
+        return null;
+    });
+
+    return {
+        lastLoadedSaveFile: validatedSaveFiles[check.lastLoadedIndex!] ?? null,
+        saveFiles: validatedSaveFiles,
+        errors,
+    };
 }
 
 function mxnLabeled(obj: Container & MxnSpeaker, label: string) {
