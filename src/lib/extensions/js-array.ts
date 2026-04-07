26e0a72f7df4daf7a094583f258a419fa971a438
hubol <hubol.gordon@gmail.com>
2026-04-07 10:54:29 -0500

    Array.uniqueBy

--- src/lib/extensions/js-array.ts
+++ src/lib/extensions/js-array.ts
@@ -1,7 +1,13 @@
+import { Pojo } from "../types/pojo";
+
+type IdentityProvider<T> = (item: T) => string | Pojo;
+
 declare global {
     interface Array<T> {
         last: T;
         removeFirst(value: T): void;
+        /** Invokes JSON.stringify when the provider returns a Pojo. Be careful to not pass cyclical objects. */
+        uniqueBy(identityProvider: IdentityProvider<T>): Array<T>;
     }
 }
 
@@ -21,6 +27,23 @@ Object.defineProperties(Array.prototype, {
             if (index > -1) {
                 this.splice(index, 1);
             }
+        },
+        configurable: true,
+    },
+    uniqueBy: {
+        value: function<T> (this: Array<T>, identityProvider: IdentityProvider<T>) {
+            const result = new Array<T>();
+            const identities = new Set<string>();
+            for (let i = 0; i < this.length; i++) {
+                const item = this[i];
+                const identity = identityProvider(item);
+                const identityString = typeof identity === "string" ? identity : JSON.stringify(identity);
+                if (!identities.has(identityString)) {
+                    identities.add(identityString);
+                    result.push(item);
+                }
+            }
+            return result;
         },
         configurable: true,
     },
