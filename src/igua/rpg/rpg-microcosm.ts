a50b2c9ce8e67805519f15f3db8a4f7033401f29
hubol <hubol.gordon@gmail.com>
2026-04-21 10:11:12 -0500

    Rename

--- src/igua/rpg/rpg-microcosm.ts
+++ src/igua/rpg/rpg-microcosm.ts
@@ -1,10 +1,11 @@
-export class RpgMicrocosmUnsafeBase {
-    public _state: any = null;
+export abstract class RpgMicrocosmUnsafeBase {
+    _state: any = null;
+    abstract createState(): any;
 }
 
 // @ts-expect-error Also don't care
 export abstract class RpgMicrocosm<TState> extends RpgMicrocosmUnsafeBase {
     // @ts-expect-error Don't care
     protected readonly _state: TState;
-    abstract createState(): TState;
+    protected abstract createState(): TState;
 }
--- src/igua/rpg/rpg-microcosms.ts
+++ src/igua/rpg/rpg-microcosms.ts
@@ -3,8 +3,8 @@ import { MicrocosmVase } from "./microcosms/microcosm-vase";
 import { RpgMicrocosm, RpgMicrocosmUnsafeBase } from "./rpg-microcosm";
 
 const Manifest = {
-    cactusEquipmentMaker: MicrocosmCactusEquipmentMaker,
-    vase: MicrocosmVase,
+    "VaseInhabitant.CactusEquipment": MicrocosmCactusEquipmentMaker,
+    "VaseInhabitant.Vase": MicrocosmVase,
 } satisfies Record<string, RpgMicrocosmClasslike>;
 
 export namespace RpgMicrocosms {
@@ -12,9 +12,11 @@ export namespace RpgMicrocosms {
         return Object.fromEntries(
             Object.entries(Manifest).map(([key, microcosmClass]) => {
                 const instance = new microcosmClass();
-                (instance as any as RpgMicrocosmUnsafeBase)._state = key in state
-                    ? state[key]
-                    : instance.createState();
+                const unsafeInstance = instance as unknown as RpgMicrocosmUnsafeBase;
+                if (!state[key]) {
+                    state[key] = unsafeInstance.createState();
+                }
+                unsafeInstance._state = state[key];
                 return [key, instance];
             }),
         ) as unknown as Instance;
@@ -29,7 +31,7 @@ export namespace RpgMicrocosms {
     type ManifestType = typeof Manifest;
 
     type Instance = {
-        [k in keyof ManifestType]: Omit<InstanceType<ManifestType[k]>, "createState">;
+        [k in keyof ManifestType]: InstanceType<ManifestType[k]>;
     };
 }
 
