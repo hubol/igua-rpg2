7d3d7c4c7130c9e59e981bfa0019fac5fe0f835f
hubol <hubol.gordon@gmail.com>
2026-05-20 15:21:12 -0500

    Try to prevent confusing paint UI cases

--- src/igua/objects/overlay/obj-ui-acceptable-range.ts
+++ src/igua/objects/overlay/obj-ui-acceptable-range.ts
@@ -25,31 +25,13 @@ export function objUiAcceptableRange(args: ObjUiAcceptableRangeArgs) {
         },
     };
 
+    const rangeObj = new Graphics();
+    const valueObj = new Graphics();
+
     return container(
         new Graphics().beginFill(0xffffff).drawRect(0, 0, args.width, args.height),
-        new Graphics()
-            .step(self => {
-                const x0 = Math.round(Math.max(0, Math.min(args.range.min / args.max, 1)) * args.width);
-                const x1 = Math.round(Math.max(0, Math.min(args.range.max / args.max, 1)) * args.width);
-                self
-                    .clear()
-                    .beginFill(0xbbbbbb)
-                    .drawRect(x0, -1, x1 - x0, args.height + 2);
-            }),
-        new Graphics()
-            .step(self => {
-                self.clear();
-
-                if (args.value === null) {
-                    return;
-                }
-
-                const x = Math.round(Math.max(0, Math.min(args.value / args.max, 1)) * args.width);
-
-                self
-                    .beginFill(0x000000)
-                    .drawRect(x, 0, 1, args.height);
-            }),
+        rangeObj,
+        valueObj,
         Sprite.from(Tx.Ui.Checkmark)
             .tinted(0x00ff00)
             .pivoted(1, 8)
@@ -57,5 +39,31 @@ export function objUiAcceptableRange(args: ObjUiAcceptableRangeArgs) {
             .invisible()
             .step(self => self.visible = api.isValueInRange),
     )
-        .merge({ objUiAcceptableRange: api });
+        .merge({ objUiAcceptableRange: api })
+        .step(() => {
+            const x0 = Math.round(Math.max(0, Math.min(args.range.min / args.max, 1)) * args.width);
+            const x1 = Math.round(Math.max(0, Math.min(args.range.max / args.max, 1)) * args.width);
+            rangeObj
+                .clear()
+                .beginFill(0xbbbbbb)
+                .drawRect(x0, -1, x1 - x0, args.height + 2);
+
+            valueObj.clear();
+
+            if (args.value === null) {
+                return;
+            }
+
+            let x = Math.round(Math.max(0, Math.min(args.value / args.max, 1)) * args.width);
+            if (args.value < args.range.min && x >= x0) {
+                x = x0 - 1;
+            }
+            if (args.value > args.range.max && x < x1) {
+                x = x1;
+            }
+
+            valueObj
+                .beginFill(0x000000)
+                .drawRect(x, 0, 1, args.height);
+        });
 }
