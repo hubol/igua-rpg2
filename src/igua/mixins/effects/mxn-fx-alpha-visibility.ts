53bacd5bc2daf0daedb3ccb219ee380992af2fc1
hubol <hubol.gordon@gmail.com>
2026-05-23 11:39:12 -0500

    `mxnFxAlphaVisibility` supports containers with alphamaskfilter

--- src/igua/mixins/effects/mxn-fx-alpha-visibility.ts
+++ src/igua/mixins/effects/mxn-fx-alpha-visibility.ts
@@ -1,14 +1,26 @@
-import { DisplayObject } from "pixi.js";
+import { AlphaFilter, BitmapFont, DisplayObject, Sprite } from "pixi.js";
 import { approachLinear } from "../../../lib/math/number";
 
 export function mxnFxAlphaVisibility(obj: DisplayObject, visible = true) {
+    const useAlphaDirectly = obj instanceof Sprite
+        || obj instanceof BitmapFont;
+
+    const alphaTarget = useAlphaDirectly ? obj : createAlphaFilter(obj);
+
     const mxnFxAlphaVisibility = {
         visible,
     };
 
-    obj.alpha = visible ? 1 : 0;
+    alphaTarget.alpha = visible ? 1 : 0;
 
     return obj
         .merge({ mxnFxAlphaVisibility })
-        .step(self => self.alpha = approachLinear(self.alpha, mxnFxAlphaVisibility.visible ? 1 : 0, 0.05));
+        .step(() => alphaTarget.alpha = approachLinear(alphaTarget.alpha, mxnFxAlphaVisibility.visible ? 1 : 0, 0.05));
+}
+
+function createAlphaFilter(obj: DisplayObject) {
+    obj.filters ??= [];
+    const alphaFilter = new AlphaFilter();
+    obj.filters.push(alphaFilter);
+    return alphaFilter;
 }
