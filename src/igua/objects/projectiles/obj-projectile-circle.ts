c092fa83971ef064457a91e1fd7cdea852974c39
hubol <hubol.gordon@gmail.com>
2026-06-11 22:30:11 -0500

    projectile circle is tintable

--- src/igua/objects/projectiles/obj-projectile-circle.ts
+++ src/igua/objects/projectiles/obj-projectile-circle.ts
@@ -7,10 +7,20 @@ export function objProjectileCircle() {
     const collisionObj = objCirclebox();
     const gfx = new Graphics().beginFill(0xffffff).drawCircle(0, 0, 256).scaled(1 / 256, 1 / 256);
 
+    const api = {
+        get tint() {
+            return gfx.tint;
+        },
+        set tint(value) {
+            gfx.tint = value;
+        },
+    };
+
     return container(
         gfx,
         collisionObj,
     )
+        .merge({ objProjectileCircle: api })
         .scaled(0, 0)
         .collisionShape(CollisionShape.DisplayObjects, collisionObj.children);
 }
