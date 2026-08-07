5dcc6bdf404a9b4f6563f3a83b1011b07e9f9a1e
Hubol <hubol.gordon@gmail.com>
2026-08-07 08:57:20 -0500

    new song: lingering straw

--- src/assets/generated/music/generated-music-data.ts
+++ src/assets/generated/music/generated-music-data.ts
@@ -22,6 +22,7 @@ export const GeneratedMusicData = {
    "HomosexualFeet": { ogg: require("./ogg/homosexual-feet.ogg") },
    "InvisibleTape": { ogg: require("./ogg/invisible-tape.ogg") },
    "IronSkittle": { ogg: require("./ogg/iron-skittle.ogg") },
+   "LingeringStraw": { ogg: require("./ogg/lingering-straw.ogg") },
    "NorthernCream": { ogg: require("./ogg/northern-cream.ogg") },
    "OldSailor": { ogg: require("./ogg/old-sailor.ogg") },
    "OpenWound": { ogg: require("./ogg/open-wound.ogg") },
