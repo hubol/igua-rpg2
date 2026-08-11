a7bfe5ab3693227cf16a78a58542c5bd8f2e7487
Hubol <hubol.gordon@gmail.com>
2026-08-11 08:38:01 -0500

    WIP roaming fish behavior

--- src/igua/scenes/scn-ohio-hall-fish.ts
+++ src/igua/scenes/scn-ohio-hall-fish.ts
@@ -1,9 +1,13 @@
 import { Sprite } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { Coro } from "../../lib/game-engine/routines/coro";
 import { interp } from "../../lib/game-engine/routines/interp";
 import { sleep, sleepf } from "../../lib/game-engine/routines/sleep";
+import { Integer } from "../../lib/math/number-alias-types";
+import { Rng } from "../../lib/math/rng";
 import { DataItem } from "../data/data-item";
 import { scene } from "../globals";
+import { mxnPhysics } from "../mixins/mxn-physics";
 import { mxnSinePivot } from "../mixins/mxn-sine-pivot";
 import { objCharacterFeederFish } from "../objects/characters/obj-character-feeder-fish";
 import { objFxFormativeBurst } from "../objects/effects/obj-fx-formative-burst";
@@ -24,8 +28,8 @@ export function scnOhioHallFish() {
         .forEach(obj => obj.mixin(mxnEmptiesFishFood));
 
     [lvl.FishMarker0, lvl.FishMarker1, lvl.FishMarker2]
-        .forEach((obj, i) =>
-            objCharacterFeederFish(69 + i * 420)
+        .forEach((obj, id) =>
+            objRoamingFish(id)
                 .at(obj)
                 .show()
         );
@@ -47,6 +51,32 @@ export function scnOhioHallFish() {
                 enemyObj = objAngelMiffed("level4")
                     .at(position)
                     .show();
+            }
+        });
+}
+
+function objRoamingFish(id: Integer) {
+    return objCharacterFeederFish(69 + id * 420)
+        .mixin(mxnPhysics, { gravity: 0, physicsRadius: 10 })
+        .coro(function* (self) {
+            let dir = Rng.intp();
+            while (true) {
+                self.scale.x *= 0.67;
+                yield sleep(200);
+                self.scale.x = -dir;
+                yield sleep(200);
+                yield interp(self.speed, "x").to(dir * 1).over(500);
+                yield* Coro.race([
+                    Coro.chain([self.mxnPhysics.dramaHitWall(), sleep(200)]),
+                    sleep(Rng.int(1000, 4000)),
+                ]);
+                if (self.speed.x !== 0) {
+                    yield interp(self.speed, "x").to(0).over(500);
+                    dir = Rng.intp();
+                }
+                else {
+                    dir *= -1;
+                }
             }
         });
 }
