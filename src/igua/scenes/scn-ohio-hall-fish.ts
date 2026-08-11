eb036523bcbb8a35ec330f045d1eca6cf12e1fd3
Hubol <hubol.gordon@gmail.com>
2026-08-11 08:17:39 -0500

    Fish food can be shaken, but nothing happens

--- src/igua/scenes/scn-ohio-hall-fish.ts
+++ src/igua/scenes/scn-ohio-hall-fish.ts
@@ -1,11 +1,26 @@
+import { Sprite } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
-import { sleep } from "../../lib/game-engine/routines/sleep";
+import { interp } from "../../lib/game-engine/routines/interp";
+import { sleep, sleepf } from "../../lib/game-engine/routines/sleep";
+import { DataItem } from "../data/data-item";
 import { scene } from "../globals";
+import { mxnSinePivot } from "../mixins/mxn-sine-pivot";
 import { objFxFormativeBurst } from "../objects/effects/obj-fx-formative-burst";
 import { objAngelMiffed } from "../objects/enemies/obj-angel-miffed";
+import { playerObj } from "../objects/obj-player";
+import { Rpg } from "../rpg/rpg";
+import { RpgInventory } from "../rpg/rpg-inventory";
 
 export function scnOhioHallFish() {
     const lvl = Lvl.OhioHallFish();
+
+    lvl.WaterRippleGroup
+        .children
+        .forEach(obj => obj.mixin(mxnSinePivot));
+
+    lvl.ArrowDownGroup
+        .children
+        .forEach(obj => obj.mixin(mxnEmptiesFishFood));
 
     scene.stage
         .coro(function* () {
@@ -16,7 +31,7 @@ export function scnOhioHallFish() {
 
             while (true) {
                 yield () => enemyObj.destroyed;
-                yield sleep(1000);
+                yield sleep(3000);
                 objFxFormativeBurst()
                     .at(position)
                     .show();
@@ -24,6 +39,35 @@ export function scnOhioHallFish() {
                 enemyObj = objAngelMiffed("level4")
                     .at(position)
                     .show();
+            }
+        });
+}
+
+function mxnEmptiesFishFood(obj: Sprite) {
+    return obj
+        .coro(function* () {
+            const item: RpgInventory.Item = { kind: "pocket_item", id: "FishFood" };
+
+            while (true) {
+                yield () => Rpg.inventory.count(item) > 0 && playerObj.collides(obj);
+                Rpg.inventory.remove(item, 1);
+
+                const figureObj = DataItem.getFigureObj(item)
+                    .pivotedUnit(0.5, 0.5)
+                    .at(obj.getWorldCenter())
+                    .add(0, -16)
+                    .show();
+
+                yield sleep(200);
+                yield interp(figureObj, "angle").steps(4).to(180).over(250);
+                yield sleep(200);
+
+                for (let i = 0; i < 10; i++) {
+                    figureObj.y += i % 2 === 0 ? 3 : -3;
+                    yield sleepf(9);
+                }
+
+                figureObj.destroy();
             }
         });
 }
