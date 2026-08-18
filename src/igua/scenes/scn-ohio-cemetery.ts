5c069d1d5014853ee17a4b92d5778410b424afa4
Hubol <hubol.gordon@gmail.com>
2026-08-18 07:53:34 -0500

    WIP ectoplasm activity

--- src/igua/scenes/scn-ohio-cemetery.ts
+++ src/igua/scenes/scn-ohio-cemetery.ts
@@ -1,6 +1,13 @@
+import { Container } from "pixi.js";
 import { Lvl } from "../../assets/generated/levels/generated-level-data";
+import { RgbInt } from "../../lib/math/number-alias-types";
+import { Rng } from "../../lib/math/rng";
+import { container } from "../../lib/pixi/container";
 import { mxnRpgKill } from "../mixins/mxn-rpg-kill";
 import { mxnSinePivot } from "../mixins/mxn-sine-pivot";
+import { mxnSparkling } from "../mixins/mxn-sparkling";
+import { playerObj } from "../objects/obj-player";
+import { Search } from "../utils/search";
 
 export function scnOhioCemetery() {
     const lvl = Lvl.OhioCemetery();
@@ -9,4 +16,43 @@ export function scnOhioCemetery() {
         .forEach(obj => obj.mixin(mxnSinePivot));
 
     lvl.PlayerKillRegion.mixin(mxnRpgKill);
+    enrichEctoplasmActivity(0xff007a);
+}
+
+function enrichEctoplasmActivity(regionTint: RgbInt) {
+    const headstoneObjs = Search.findRegions(regionTint)
+        .map(mxnHeadstone);
+
+    container()
+        .coro(function* () {
+            let index = Rng.int(headstoneObjs.length);
+            while (true) {
+                const headstoneObj = headstoneObjs[index];
+                headstoneObj.sparklesPerFrame = 0.3;
+                yield () => headstoneObj.mxnHeadstone.isPlayerPerched;
+                headstoneObj.sparklesPerFrame = 0;
+                if (index === 0) {
+                    index += 1;
+                }
+                else if (index === headstoneObjs.length - 1) {
+                    index -= 1;
+                }
+                else {
+                    index += Rng.intp();
+                }
+            }
+        })
+        .show();
+}
+
+function mxnHeadstone(obj: Container) {
+    const api = {
+        get isPlayerPerched() {
+            return playerObj.y < obj.y + obj.height && playerObj.isOnGround && playerObj.collides(obj);
+        },
+    };
+
+    return obj
+        .merge({ mxnHeadstone: api })
+        .mixin(mxnSparkling);
 }
